23bc59d3a56d860c65547278ffebcbe0c9bc85e5
miguelinux <mhernandez314@gmail.com>
2020-05-29 11:25:13 +0200

    FAPEC codec: split no-prediction and linear-prediction in two classes
    Former-commit-id: 8a21c3650a7a44dab0ca1e5a4497117bf98f7dcf

--- plugins/plugin_fapec/fapec_codec.py
+++ plugins/plugin_fapec/fapec_codec.py
@@ -12,8 +12,7 @@ from enb.config import get_options
 
 options = get_options()
 
-
-class FAPEC_NP(icompression.LossyCodec, icompression.LosslessCodec, icompression.WrapperCodec):
+class FAPEC_Abstract(icompression.LossyCodec, icompression.LosslessCodec, icompression.WrapperCodec):
     """Wrapper for FAPEC with no preprocessing (-np option).
     """
     BAND_FORMAT_BIP, BAND_FORMAT_BIL, BAND_FORMAT_BSQ, BAND_FORMAT_BAYER = range(4)
@@ -46,6 +45,9 @@ class FAPEC_NP(icompression.LossyCodec, icompression.LosslessCodec, icompression
                          decompressor_path=os.path.join(bin_dir, "unfapec"),
                          param_dict=param_dict, output_invocation_dir=output_invocation_dir)
 
+    def get_transform_dict_params(self, original_file_info):
+        raise NotImplementedError("Please select one of the subclasses")
+
     def get_dtype(self, original_file_info):
         return 8 * original_file_info["bytes_per_sample"]
 
@@ -55,8 +57,8 @@ class FAPEC_NP(icompression.LossyCodec, icompression.LosslessCodec, icompression
         invocation_params = dict(self.param_dict)
         invocation_params.update(
             hd=0, chd=0,
-            dtype=self.get_dtype(original_file_info=original_file_info),
-            np=("tc" if original_file_info["signed"] else "us"))
+            dtype=self.get_dtype(original_file_info=original_file_info))
+        invocation_params.update(self.get_transform_dict_params(original_file_info=original_file_info))
         invocation = " ".join(f"-{k} {v}" for k, v in invocation_params.items())
         if original_file_info["signed"]:
             invocation += " -signed"
@@ -69,3 +71,44 @@ class FAPEC_NP(icompression.LossyCodec, icompression.LosslessCodec, icompression
     def get_decompression_params(self, compressed_path, reconstructed_path, original_file_info):
         invocation = f"-o {reconstructed_path} -ow {compressed_path}"
         return invocation
+
+
+
+class FAPEC_NP(FAPEC_Abstract):
+    """Wrapper for FAPEC with no preprocessing (-np option).
+    """
+    def get_transform_dict_params(self, original_file_info):
+        return dict(np=("tc" if original_file_info["signed"] else "us"))
+
+    @property
+    def label(self):
+        return "FAPEC-NP"
+
+class FAPEC_LP(FAPEC_Abstract):
+    """Wrapper for FAPEC with "spatial" lineal prediction
+    """
+    def __init__(self, linear_prediction_order=1,
+                 bin_dir=None, chunk_size_str="64M", threads=1,
+                 lsb_discard_count=0,
+                 adaptiveness_block_length=64,
+                 output_invocation_dir=None):
+        super().__init__(bin_dir=bin_dir, chunk_size_str=chunk_size_str,
+                         threads=threads, lsb_discard_count=lsb_discard_count,
+                         adaptiveness_block_length=adaptiveness_block_length,
+                         output_invocation_dir=output_invocation_dir)
+        assert linear_prediction_order >= 0
+        assert linear_prediction_order == int(linear_prediction_order)
+        self.linear_prediction_order = linear_prediction_order
+
+    linear_prediction_oder = 1
+
+    def get_transform_dict_params(self, original_file_info):
+        assert self.linear_prediction_oder >= 1
+        if self.linear_prediction_order == 1:
+            return dict()
+        else:
+            return dict(od=self.linear_prediction_oder)
+
+    @property
+    def label(self):
+        return "FAPEC-LP"
