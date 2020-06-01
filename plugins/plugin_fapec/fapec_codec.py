9dc4d42a471db43f738534e5b8e215fd974a67bd
miguelinux <mhernandez314@gmail.com>
2020-06-01 18:02:43 +0200

    Added FAPEC with spectral codecs

--- plugins/plugin_fapec/fapec_codec.py
+++ plugins/plugin_fapec/fapec_codec.py
@@ -12,6 +12,7 @@ from enb.config import get_options
 
 options = get_options()
 
+
 class FAPEC_Abstract(icompression.LossyCodec, icompression.LosslessCodec, icompression.WrapperCodec):
     """Wrapper for FAPEC with no preprocessing (-np option).
     """
@@ -52,8 +53,6 @@ class FAPEC_Abstract(icompression.LossyCodec, icompression.LosslessCodec, icompr
         return 8 * original_file_info["bytes_per_sample"]
 
     def get_compression_params(self, original_path, compressed_path, original_file_info):
-        """Get the dictionary of parameters to use in the -x k=v options of lcnl_header_tool.
-        """
         invocation_params = dict(self.param_dict)
         invocation_params.update(
             hd=0, chd=0,
@@ -73,10 +72,10 @@ class FAPEC_Abstract(icompression.LossyCodec, icompression.LosslessCodec, icompr
         return invocation
 
 
-
 class FAPEC_NP(FAPEC_Abstract):
     """Wrapper for FAPEC with no preprocessing (-np option).
     """
+
     def get_transform_dict_params(self, original_file_info):
         return dict(np=("tc" if original_file_info["signed"] else "us"))
 
@@ -84,9 +83,11 @@ class FAPEC_NP(FAPEC_Abstract):
     def label(self):
         return "FAPEC-NP"
 
+
 class FAPEC_LP(FAPEC_Abstract):
     """Wrapper for FAPEC with "spatial" lineal prediction
     """
+
     def __init__(self, linear_prediction_order=1,
                  bin_dir=None, chunk_size_str="64M", threads=1,
                  lsb_discard_count=0,
@@ -110,5 +111,69 @@ class FAPEC_LP(FAPEC_Abstract):
             return dict(od=self.linear_prediction_oder)
 
     @property
+    def name(self):
+        return super().name + (f"_od={self.linear_prediction_order}" if self.linear_prediction_order > 1 else "")
+
+    @property
     def label(self):
         return "FAPEC-LP"
+
+
+class FAPEC_DWT(FAPEC_Abstract):
+    def __init__(self, dwt_losses=0,
+                 bin_dir=None, chunk_size_str="64M", threads=1,
+                 lsb_discard_count=0,
+                 adaptiveness_block_length=64,
+                 output_invocation_dir=None):
+        super().__init__(bin_dir=bin_dir, chunk_size_str=chunk_size_str,
+                         threads=threads, lsb_discard_count=lsb_discard_count,
+                         adaptiveness_block_length=adaptiveness_block_length,
+                         output_invocation_dir=output_invocation_dir)
+        self.dwt_losses = dwt_losses
+        assert self.dwt_losses >= 0
+
+    @property
+    def name(self):
+        return super().name + f"_dwt_losses={self.dwt_losses}"
+
+    @property
+    def label(self):
+        return f"FAPEC-DWT"
+
+    def get_transform_dict_params(self, original_file_info):
+        return dict(dwt=f"{original_file_info['width']} "
+                        f"{original_file_info['height']} "
+                        f"{original_file_info['component_count']} "
+                        f"{self.dwt_losses} "
+                        f"{original_file_info['dynamic_range_bits']} "
+                        f"{2}") # <bfmt>    Bands format: 0=BIP, 1=BIL, 2=BSQ, 3=Bayer
+
+
+class FAPEC_HPA(FAPEC_Abstract):
+    def __init__(self, hpa_losses=0,
+                 bin_dir=None, chunk_size_str="64M", threads=1,
+                 lsb_discard_count=0,
+                 adaptiveness_block_length=64,
+                 output_invocation_dir=None):
+        super().__init__(bin_dir=bin_dir, chunk_size_str=chunk_size_str,
+                         threads=threads, lsb_discard_count=lsb_discard_count,
+                         adaptiveness_block_length=adaptiveness_block_length,
+                         output_invocation_dir=output_invocation_dir)
+        self.hpa_losses = hpa_losses
+        assert 0 <= self.hpa_losses <= 16
+
+    @property
+    def name(self):
+        return super().name + f"_hpa_losses={self.hpa_losses}"
+
+    @property
+    def label(self):
+        return f"FAPEC-HPA"
+
+    def get_transform_dict_params(self, original_file_info):
+        return dict(hpa=f"{original_file_info['width']} "
+                        f"{original_file_info['height']} "
+                        f"{original_file_info['component_count']} "
+                        f"{self.hpa_losses} "
+                        f"{original_file_info['dynamic_range_bits']} "
+                        f"{2}") # <bfmt>    Bands format: 0=BIP, 1=BIL, 2=BSQ, 3=Bayer
