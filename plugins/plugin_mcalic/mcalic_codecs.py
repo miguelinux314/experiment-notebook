3593cd249c3f59e62b519a4f19a4812262ed5e1d
miguelinux <mhernandez314@gmail.com>
2020-06-16 13:31:10 +0200

    m-calic codec now reports time correctly

--- plugins/plugin_mcalic/mcalic_codecs.py
+++ plugins/plugin_mcalic/mcalic_codecs.py
@@ -32,7 +32,7 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
     split_height_count = 3
     split_width_count = 3
 
-    def __init__(self, max_error=0, bin_dir=None, data_format=None):
+    def __init__(self, max_error=0, bin_dir=None, data_format=None, output_invocation_dir=None):
         """
         :param max_error: maximum pixelwise error allowed. Use 0 for lossless
           compression
@@ -57,11 +57,12 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
         icompression.WrapperCodec.__init__(
             self, compressor_path=os.path.join(bin_dir, "Mcalic_enc_nl"),
             decompressor_path=os.path.join(bin_dir, "Mcalic_dec_nl"),
-            param_dict=param_dict)
+            param_dict=param_dict, output_invocation_dir=output_invocation_dir)
 
     def compress(self, original_path: str, compressed_path: str, original_file_info=None):
         # Tested limit: self.max_tested_spatial_size
-        if original_file_info["width"] <= self.max_dimension_size and original_file_info["height"] <= self.max_dimension_size:
+        if original_file_info["width"] <= self.max_dimension_size \
+                and original_file_info["height"] <= self.max_dimension_size:
             return self.compress_one(original_path=original_path,
                                      compressed_path=compressed_path,
                                      original_file_info=original_file_info)
@@ -70,6 +71,7 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
             img = isets.load_array_bsq(file_or_path=original_path, image_properties_row=original_file_info)
             with tempfile.TemporaryDirectory(dir=options.base_tmp_dir) as tmp_dir:
                 compound_size = 0
+                total_compression_time = 0
                 for y in range(self.split_height_count):
                     for x in range(self.split_width_count):
                         small_array = \
@@ -88,9 +90,10 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
                         small_file_info = copy.copy(original_file_info)
                         small_file_info["width"], small_file_info["height"], small_file_info[
                             "component_count"] = small_array.shape
-                        self.compress_one(
+                        compression_results = self.compress_one(
                             original_path=small_path, compressed_path=small_compressed_path,
                             original_file_info=small_file_info)
+                        total_compression_time += compression_results.compression_time_seconds
                         tl_writer.add_file(small_compressed_path)
                         os.remove(small_path)
                         compound_size += small_array.size
@@ -99,7 +102,14 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
                     "samples"], f"compound_size = {compound_size} != {original_file_info['samples']} = original samples"
                 tl_writer.write(output_path=compressed_path)
 
+                compression_results = self.compression_results_from_paths(original_path=original_path,
+                                                                          compressed_path=compressed_path)
+                compression_results.compression_time_seconds = total_compression_time
+                return compression_results
+
     def compress_one(self, original_path: str, compressed_path: str, original_file_info=None):
+        """Compress one image tile with M-CALIC.
+        """
         assert original_file_info["bytes_per_sample"] == 2, \
             f"This implementation of M-CALIC ({self.compressor_path}) only supports 16bpp"
         with tempfile.NamedTemporaryFile(
@@ -111,7 +121,7 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
 
             offset = None
             if original_file_info["signed"]:
-                offset, original_max = img.min(), img.max()
+                offset, original_max = int(img.min()), int(img.max())
                 offset = min(offset, 0)
                 assert original_max - offset <= 2 ** 15 - 1, \
                     f"Invalid dynamic range of signed image ({offset}, {original_max})"
@@ -130,13 +140,20 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
                                                     suffix=".txt", mode="w") as si_file:
                     si_file.write(f"{abs(offset):d}")
                     si_file.flush()
-                    super().compress(original_path=bil_le_file.name, compressed_path=tmp_compressed_file.name,
-                                     original_file_info=original_file_info)
+                    compression_results = super().compress(original_path=bil_le_file.name,
+                                                           compressed_path=tmp_compressed_file.name,
+                                                           original_file_info=original_file_info)
                     tarlite.TarliteWriter(initial_input_paths=[si_file.name, tmp_compressed_file.name]).write(
                         compressed_path)
+                    compression_results.original_path=original_path
+                    compression_results.compressed_path = compressed_path
+                    return compression_results
             else:
-                super().compress(original_path=bil_le_file.name, compressed_path=compressed_path,
-                                 original_file_info=original_file_info)
+                compression_results = super().compress(
+                    original_path=bil_le_file.name, compressed_path=compressed_path,
+                    original_file_info=original_file_info)
+                compression_results.original_path = original_path
+                return compression_results
 
     def get_compression_params(self, original_path, compressed_path, original_file_info):
         return f"{original_path} {compressed_path} " \
@@ -144,7 +161,8 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
                f"16 {self.param_dict['max_error']} {self.param_dict['data_format']}"
 
     def decompress(self, compressed_path, reconstructed_path, original_file_info=None):
-        if original_file_info["width"] <= self.max_dimension_size and original_file_info["height"] <= self.max_dimension_size:
+        if original_file_info["width"] <= self.max_dimension_size and original_file_info[
+            "height"] <= self.max_dimension_size:
             return self.decompress_one(compressed_path=compressed_path,
                                        reconstructed_path=reconstructed_path,
                                        original_file_info=original_file_info)
@@ -153,6 +171,7 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
             img = np.zeros(
                 (original_file_info["width"], original_file_info["height"], original_file_info["component_count"]),
                 dtype=isets.iproperties_row_to_numpy_dtype(image_properties_row=original_file_info))
+            total_decompression_time = 0
             with tempfile.TemporaryDirectory(dir=options.base_tmp_dir) as tmp_dir:
                 tl_reader.extract_all(output_dir_path=tmp_dir)
                 invocation = f"ls -lah {tmp_dir}"
@@ -178,8 +197,9 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
                             else original_file_info["width"] - (self.split_width_count - 1) * (
                                 original_file_info["width"] // self.split_width_count)
 
-                        self.decompress_one(compressed_path=small_compressed_path, reconstructed_path=small_path,
-                                            original_file_info=small_file_info)
+                        dr = self.decompress_one(compressed_path=small_compressed_path, reconstructed_path=small_path,
+                                                 original_file_info=small_file_info)
+                        total_decompression_time += dr.decompression_time_seconds
                         small_array = isets.load_array_bsq(file_or_path=small_path,
                                                            image_properties_row=small_file_info)
                         img[x * (original_file_info["width"] // self.split_width_count):
@@ -191,10 +211,15 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
                                          "height"] // self.split_height_count)) if y < self.split_height_count - 1 else
                          original_file_info["height"]),
                         :] = small_array
-
                 isets.dump_array_bsq(array=img, file_or_path=reconstructed_path)
 
+            decompression_results = self.decompression_results_from_paths(
+                compressed_path=compressed_path, reconstructed_path=reconstructed_path)
+            decompression_results.decompression_time_seconds = total_decompression_time
+            return decompression_results
+
     def decompress_one(self, compressed_path, reconstructed_path, original_file_info=None):
+        total_decompression_time = 0
         with tempfile.NamedTemporaryFile(
                 dir=options.base_tmp_dir,
                 prefix=f"reconstructed_{os.path.basename(reconstructed_path)}",
@@ -219,10 +244,14 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
                     os.path.getsize(tmp_extract_dir)
                     inner_compressed_path = glob.glob(os.path.join(tmp_extract_dir, "*.mcalic"))[0]
 
-                    self.decompress_short_names(compressed_path=inner_compressed_path, reconstructed_path=bil_le_file.name, original_file_info=original_file_info)
+                    dr = self.decompress_short_names(compressed_path=inner_compressed_path,
+                                                     reconstructed_path=bil_le_file.name,
+                                                     original_file_info=original_file_info)
+                    total_decompression_time += dr.decompression_time_seconds
             else:
-                self.decompress_short_names(compressed_path=compressed_path, reconstructed_path=bil_le_file.name,
-                                            original_file_info=original_file_info)
+                dr = self.decompress_short_names(compressed_path=compressed_path, reconstructed_path=bil_le_file.name,
+                                                 original_file_info=original_file_info)
+                total_decompression_time += dr.decompression_time_seconds
 
             original_dtype = isets.iproperties_row_to_numpy_dtype(image_properties_row=original_file_info)
 
@@ -247,6 +276,11 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
         except FileNotFoundError:
             pass
 
+        decompression_results = self.decompression_results_from_paths(compressed_path=compressed_path,
+                                                                      reconstructed_path=reconstructed_path)
+        decompression_results.decompression_time_seconds = total_decompression_time
+        return decompression_results
+
     def decompress_short_names(self, compressed_path, reconstructed_path, original_file_info):
         """Binary seems to have problems with too long file names
         """
@@ -254,10 +288,13 @@ class MCALIC_Magli(icompression.NearLosslessCodec, icompression.WrapperCodec):
         r_path = f"{random.randint(0, 10000000)}.raw"
         try:
             shutil.copyfile(compressed_path, c_path)
-            super().decompress(compressed_path=c_path,
-                               reconstructed_path=r_path,
-                               original_file_info=original_file_info)
+            decompression_results = super().decompress(compressed_path=c_path,
+                                                       reconstructed_path=r_path,
+                                                       original_file_info=original_file_info)
             shutil.copyfile(r_path, reconstructed_path)
+            decompression_results.compressed_path = compressed_path
+            decompression_results.reconstructed_path = reconstructed_path
+            return decompression_results
         finally:
             for p in (c_path, r_path):
                 if os.path.exists(p):
