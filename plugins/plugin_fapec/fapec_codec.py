bb3aac5a1a952978b3be178198f59ca24237d0bf
miguelinux <mhernandez314@gmail.com>
2020-09-13 09:05:00 +0200

    Added Fapec plugin wrapper for the CILLIC mode
    Former-commit-id: 3205fe7df8e94126f79ad2d1302740eaf7af9261

--- plugins/plugin_fapec/fapec_codec.py
+++ plugins/plugin_fapec/fapec_codec.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Wrappers for the FAPEC codec
+"""Wrappers for the FAPEC codec.
+
+2020/09/07: added support for the CILLIC algorithm
 """
 __author__ = "Miguel Hernández Cabronero <miguel.hernandez@uab.cat>"
 __date__ = "25/05/2020"
@@ -14,7 +16,7 @@ options = get_options()
 
 
 class FAPEC_Abstract(icompression.LossyCodec, icompression.LosslessCodec, icompression.WrapperCodec):
-    """Wrapper for FAPEC with no preprocessing (-np option).
+    """Base class for FAPEC coders
     """
     BAND_FORMAT_BIP, BAND_FORMAT_BIL, BAND_FORMAT_BSQ, BAND_FORMAT_BAYER = range(4)
     default_band_format = BAND_FORMAT_BSQ
@@ -24,12 +26,10 @@ class FAPEC_Abstract(icompression.LossyCodec, icompression.LosslessCodec, icompr
                  adaptiveness_block_length=128,
                  output_invocation_dir=None):
         """
-        :param bin_dir:
-        :param chunk_size_str:
-        :param threads:
-        :param prediction_band_count: number of bands used for multi-component prediction
-        :param lsb_discard_count:  number of LSBs discarded
-        :param adaptiveness_block_length:
+        :param bin_dir: path to the dir with the fapec and unfapec binaries
+        :param chunk_size_str: string to be passed in the -chunk argument
+        :param threads: number of threads to use for compression/decompression
+        :param output_invocation_dir
         """
         param_dict = dict()
         param_dict["chunk"] = chunk_size_str
@@ -107,7 +107,7 @@ class FAPEC_MB(FAPEC_Abstract):
 
 
 class FAPEC_LP(FAPEC_Abstract):
-    """Wrapper for FAPEC with "spatial" lineal prediction
+    """Wrapper for FAPEC with "spatial" lineal prediction (-lp flag)
     """
 
     def __init__(self, linear_prediction_order=1,
@@ -142,6 +142,8 @@ class FAPEC_LP(FAPEC_Abstract):
 
 
 class FAPEC_DWT(FAPEC_Abstract):
+    """Run FAPEC with the spectral+spatial IWT (-dwt flag).
+    """
     def __init__(self, dwt_losses=0,
                  bin_dir=None, chunk_size_str="128M", threads=1,
                  lsb_discard_count=0,
@@ -169,6 +171,39 @@ class FAPEC_DWT(FAPEC_Abstract):
                         f"{self.dwt_losses} "
                         f"{original_file_info['dynamic_range_bits']} "
                         f"{2}") # <bfmt>    Bands format: 0=BIP, 1=BIL, 2=BSQ, 3=Bayer
+
+class FAPEC_CILLIC(FAPEC_Abstract):
+    """Run fapec with the CILLIC option.
+    """
+    def __init__(self, losses=0, multiband_levels=6,
+                 bin_dir=None, chunk_size_str="128M", threads=1,
+                 lsb_discard_count=0,
+                 adaptiveness_block_length=128,
+                 output_invocation_dir=None):
+        super().__init__(bin_dir=bin_dir, chunk_size_str=chunk_size_str,
+                         threads=threads, lsb_discard_count=lsb_discard_count,
+                         adaptiveness_block_length=adaptiveness_block_length,
+                         output_invocation_dir=output_invocation_dir)
+        self.losses = losses
+        self.multiband_levels = multiband_levels
+        assert self.losses >= 0
+
+    @property
+    def name(self):
+        return super().name + f"_losses={self.losses}_lev={self.multiband_levels}"
+
+    @property
+    def label(self):
+        return f"FAPEC-CILLIC"
+
+    def get_transform_dict_params(self, original_file_info):
+        return dict(cillic=f"{original_file_info['width']} "
+                        f"{original_file_info['height']} "
+                        f"{original_file_info['component_count']} "
+                        f"{self.losses} "
+                        f"{original_file_info['dynamic_range_bits']} "
+                        f"{2}", # <bfmt>    Bands format: 0=BIP, 1=BIL, 2=BSQ, 3=Bayer
+                    lev=self.multiband_levels)
 
 
 class FAPEC_2DDWT(FAPEC_DWT):
