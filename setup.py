fdb4b3851aea0998d27f32b159d06538e6bc81cd
miguelinux <mhernandez314@gmail.com>
2020-09-13 13:30:48 +0200

    Added setup.py
    Former-commit-id: e77264bad8db2dcb62d3eb2174f1c76cbb91fdf0

---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# ===================
+# Experiment Notebook setup
+# ===================
+#
+# Author: Miguel Hernández Cabronero <miguel.hernandez@uab.cat>
+#
+# ---------------------------------------------
+# INSTALLATION
+#
+# To install for all users, run
+#
+# $ sudo python setup.py  install
+#
+# To install for the current user only, run
+#
+# $ python setup.py  install --user
+# ---------------------------------------------
+#
+# See documentation for more information and usage examples.
+#
+
+
+# Taken from https://www.jeffknupp.com/blog/2013/08/16/open-sourcing-a-python-project-the-right-way/
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import io
+import codecs
+import os
+import sys
+import glob
+
+import enb
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
+
+setup(
+    name='enb',
+    version=0.1,
+    url='https://github.com/miguelinux314/experiment-notebook',
+    license='MIT License',
+    author='Miguel Hernandez Cabronero (Universitat Autònoma de Barcelona)',
+    install_requires=['pandas', 'ray', 'matplotlib', 'numpy', 'recordclass', 'sortedcontainers'],
+    author_email='miguel.hernandez@uab.cat',
+    description='Library to gather and disseminate computer-based experimental results.',
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        ],
+)
