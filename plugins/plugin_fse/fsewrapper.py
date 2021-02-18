82990a6d564705d198cb0e5169d3f18bd8a9f00e
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2021-02-18 09:05:19 +0100

    Merged Natalia's plugin.
    Former-commit-id: 9931a5ac129907e21517d4e6c016efde9bf4bd95

--- plugins/plugin_fse/fsewrapper.py
+++ plugins/plugin_fse/fsewrapper.py
@@ -1,19 +1,19 @@
-
+import os
 from enb import icompression
 from enb.config import get_options
-
 
 options = get_options()
 
 class FSEWrapper(icompression.WrapperCodec, icompression.LosslessCodec):
     def __init__(self):
-        icompression.WrapperCodec.__init__(self, compressor_path=f"./plugin_fse/fse",
-                                           decompressor_path=f"./plugin_fse/fse", param_dict=None,
-                                           output_invocation_dir=None)
-
+        icompression.WrapperCodec.__init__(
+            self,
+            compressor_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "fse"),
+            decompressor_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "fse"),
+            param_dict=None, output_invocation_dir=None)
 
     def get_compression_params(self, original_path, compressed_path, original_file_info):
-        return f" -e -f {original_path} {compressed_path}"
+        return f"-e -f {original_path} {compressed_path}"
 
     def get_decompression_params(self, compressed_path, reconstructed_path, original_file_info):
-        return f" -e -d -f {compressed_path} {reconstructed_path}"
+        return f"-e -d -f {compressed_path} {reconstructed_path}"
