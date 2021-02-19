aedf141c039b1e805316ca63df0063782e02cd84
Ester Jara Lorente <44998512+esterjara@users.noreply.github.com>
2021-02-19 21:07:27 +0100

    Update huffman_codec.py
    Former-commit-id: 7c44fe41f16b250639d90f13971c5ac1be662851

--- plugins/plugin_huffman/huffman_codec.py
+++ plugins/plugin_huffman/huffman_codec.py
@@ -10,12 +10,11 @@ class Huffman(icompression.WrapperCodec, icompression.LosslessCodec):
 			compressor_path=huff_binary,
 			decompressor_path=huff_binary,
 			param_dict=None, output_invocation_dir=None)
+		
 		self.huff_binary = huff_binary
+		
 		assert os.path.isfile(huff_binary)
-		'''
-		icompression.WrapperCodec.__init__(self, compressor_path=f"./plugin_huffman/Huff",
-                decompressor_path=f"./plugin_huffman/Huff", param_dict=None, output_invocation_dir=None)
-		'''
+
 	def get_compression_params(self, original_path, compressed_path, original_file_info):
 		return f"-h -f {original_path} {compressed_path}"
 
