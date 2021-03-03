ae52fc473c2da68a58de780e5f6a0d9ebb92a8ae
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2021-03-03 18:29:20 +0100

    Fixed plugin imports
    Former-commit-id: c60775c8b43ec242f1472699702dd8b23413b55f

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -6,7 +6,6 @@ from . import plugin_jpeg
 from . import plugin_jpeg_xl
 from . import plugin_lcnl
 from . import plugin_marlin
-from . import plugin_marlin_python
 from . import plugin_mcalic
 from . import plugin_mhdc_transforms
 from . import plugin_zip
