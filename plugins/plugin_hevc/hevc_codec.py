4d52b785b7524eaa343870125377950999e3ab63
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2021-03-10 21:28:20 +0100

    - Merge HEVC lossy from Natàlia's dev branch - Added lossy configuration file for HEVC - Added HEVC with larger QP to test_all_codecs.py - Fixed import errors in __init__

--- plugins/plugin_hevc/hevc_codec.py
+++ plugins/plugin_hevc/hevc_codec.py
@@ -39,11 +39,22 @@ class HEVC_lossless(icompression.WrapperCodec, icompression.LosslessCodec):
 
 class HEVC_lossy(icompression.WrapperCodec, icompression.LossyCodec):
     def __init__(self, config_path=None, chroma_format="400", qp=4):
+        # TODO: this class repeats a lot of codde from HEVC_lossless
+        # please  avoid duplicating code, e.g., call super().__init__() and defin
+        # other functions as necessary
+
+        # TODO: the config file did not exist. Please verify changes
+        # in hevc_lossy_400.cfg and compare them to the recommened
+        # values in https://vcgit.hhi.fraunhofer.de/jvet/HM/-/tree/master/cfg
         config_path = config_path if config_path is not None \
             else os.path.join(os.path.dirname(os.path.abspath(__file__)),
                               f"hevc_lossy_{chroma_format}.cfg")
 
+
+        # TODO: no need for sorted containers here
         param_dict = sortedcontainers.SortedDict()
+
+        # TODO: the value of QP should be validated in the __init__
         param_dict['QP'] = str(qp)
         chroma_format = str(chroma_format)
         param_dict['chroma_format'] = chroma_format
@@ -65,7 +76,8 @@ class HEVC_lossy(icompression.WrapperCodec, icompression.LossyCodec):
             return f"-i {original_path} -c {self.config_path} -b {compressed_path} -wdt {original_file_info['width']} " \
                    f"-hgt {original_file_info['height']} -f {original_file_info['component_count']} " \
                    f"-cf {self.param_dict['chroma_format']} --InputChromaFormat={self.param_dict['chroma_format']} " \
-                   f"--InputBitDepth={8 * original_file_info['bytes_per_sample']} -fr 1 -q {self.param_dict['QP']}"
+                   f"--InputBitDepth={8 * original_file_info['bytes_per_sample']} " \
+                   f"-q {self.param_dict['QP']}"
 
     def get_decompression_params(self, compressed_path, reconstructed_path, original_file_info):
         if original_file_info['bytes_per_sample'] > 1:
