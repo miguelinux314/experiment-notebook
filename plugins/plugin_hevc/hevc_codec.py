92c28ee3ebd1e6bc2b78a5f038175f0b542e48fd
nataliablasco00 <natalia.blascoa@e-campus.uab.cat>
2021-03-14 20:01:54 +0100

    HEVC fixed

--- plugins/plugin_hevc/hevc_codec.py
+++ plugins/plugin_hevc/hevc_codec.py
@@ -1,12 +1,11 @@
 import os
 from enb import icompression
 from enb.config import get_options
-import sortedcontainers
 
 options = get_options()
 
 
-class HEVC_lossless(icompression.WrapperCodec, icompression.LosslessCodec):
+class HEVC(icompression.WrapperCodec):
     def __init__(self, config_path=None, chroma_format="400"):
         config_path = config_path if config_path is not None \
             else os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -23,6 +22,8 @@ class HEVC_lossless(icompression.WrapperCodec, icompression.LosslessCodec):
 
         self.config_path = config_path
 
+
+class HEVC_lossless(icompression.LosslessCodec, HEVC):
     def get_compression_params(self, original_path, compressed_path, original_file_info):
         return f"-i {original_path} -c {self.config_path} -b {compressed_path} -wdt {original_file_info['width']} " \
                f"-hgt {original_file_info['height']} -f {original_file_info['component_count']} " \
@@ -37,40 +38,14 @@ class HEVC_lossless(icompression.WrapperCodec, icompression.LosslessCodec):
         return "HEVC lossless"
 
 
-class HEVC_lossy(icompression.WrapperCodec, icompression.LossyCodec):
+class HEVC_lossy(icompression.LossyCodec, HEVC):
     def __init__(self, config_path=None, chroma_format="400", qp=4):
-        # TODO: this class repeats a lot of codde from HEVC_lossless
-        # please  avoid duplicating code, e.g., call super().__init__() and defin
-        # other functions as necessary
-
-        # TODO: the config file did not exist. Please verify changes
-        # in hevc_lossy_400.cfg and compare them to the recommened
-        # values in https://vcgit.hhi.fraunhofer.de/jvet/HM/-/tree/master/cfg
-        config_path = config_path if config_path is not None \
-            else os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                              f"hevc_lossy_{chroma_format}.cfg")
-
-
-        # TODO: no need for sorted containers here
-        param_dict = sortedcontainers.SortedDict()
-
-        # TODO: the value of QP should be validated in the __init__
-        param_dict['QP'] = str(qp)
-        chroma_format = str(chroma_format)
-        param_dict['chroma_format'] = chroma_format
-
-        assert chroma_format in ["400"], f"Chroma format {chroma_format} not supported."
-
-        icompression.WrapperCodec.__init__(
-            self,
-            compressor_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "TAppEncoderStatic"),
-            decompressor_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "TAppDecoderStatic"),
-            param_dict=param_dict)
-
-        self.config_path = config_path
+        HEVC.__init__(self, config_path, chroma_format)
+        assert 0 <= qp <= 51
+        self.param_dict['QP'] = qp
 
     def get_compression_params(self, original_path, compressed_path, original_file_info):
-        if original_file_info['bytes_per_sample'] > 1:
+        if original_file_info['bytes_per_sample'] > 5:
             raise Exception(f"Bytes per sample = {original_file_info['bytes_per_sample']} not supported")
         else:
             return f"-i {original_path} -c {self.config_path} -b {compressed_path} -wdt {original_file_info['width']} " \
@@ -80,7 +55,7 @@ class HEVC_lossy(icompression.WrapperCodec, icompression.LossyCodec):
                    f"-q {self.param_dict['QP']}"
 
     def get_decompression_params(self, compressed_path, reconstructed_path, original_file_info):
-        if original_file_info['bytes_per_sample'] > 1:
+        if original_file_info['bytes_per_sample'] > 5:
             raise Exception(f"Bytes per sample = {original_file_info['bytes_per_sample']} not supported")
         else:
             return f"-b {compressed_path} -o {reconstructed_path} -d {8 * original_file_info['bytes_per_sample']}"
