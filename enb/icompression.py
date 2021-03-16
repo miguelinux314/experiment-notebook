259e8480cf073029c1ff7e437d3b979832927a1e
ester <ester.jara@e-campus.uab.cat>
2021-03-16 20:53:51 +0100

    Added MS-SSIM metric in templates/lossy_compression_experiment folder

--- enb/icompression.py
+++ enb/icompression.py
@@ -20,6 +20,12 @@ import imageio
 import subprocess
 import numpngw
 
+from skimage.transform import resize
+from scipy import signal
+from scipy.ndimage.filters import convolve
+from PIL import Image
+
+
 import enb
 from enb import atable
 from enb import experiment
@@ -880,6 +886,124 @@ class LossyCompressionExperiment(CompressionExperiment):
         row[_column_name] = 10 * math.log10((max_error ** 2) / row["mse"]) \
             if row["mse"] > 0 else float("inf")
 
+class SSIM(CompressionExperiment):
+    @atable.column_function("ms_ssim", label="MS-SSIM", plot_min=0)
+    def set_MultiScaleSSIM(self, index, row):
+
+        max_val = 255
+        filter_size = 11
+        filter_sigma = 1.5
+        k1 = 0.01
+        k2 = 0.03
+        weights = None
+
+        original_array = np.fromfile(row.compression_results.original_path,
+                                     dtype=row.numpy_dtype)
+        original_array = np.reshape(original_array, (row.image_info_row["width"], row.image_info_row["height"], row.image_info_row["component_count"]))
+
+        '''rawData = open(row.compression_results.original_path, 'rb').read()
+        imgSize = (row.image_info_row["width"], row.image_info_row["height"])  # the image size
+        I = Image.frombytes('I;16', imgSize, rawData)
+        original_array = np.asarray(I)'''
+
+        reconstructed_array = np.fromfile(row.decompression_results.reconstructed_path,
+                                     dtype=row.numpy_dtype)
+        reconstructed_array = np.reshape(reconstructed_array, (row.image_info_row["width"], row.image_info_row["height"], row.image_info_row["component_count"]))
+
+        '''rawData = open(row.decompression_results.reconstructed_path, 'rb').read()
+        imgSize = (row.image_info_row["width"], row.image_info_row["height"])  # the image size
+        I = Image.frombytes('I;16', imgSize, rawData)
+        reconstructed_array = np.asarray(I)'''
+
+        if original_array.shape != reconstructed_array.shape:
+            raise RuntimeError('Input images must have the same shape (%s vs. %s).',
+                               original_array.shape, reconstructed_array.shape)
+        if original_array.ndim != 3:
+            raise RuntimeError('Input images must have four dimensions, not %d',
+                               original_array.ndim)
+
+        # Note: default weights don't sum to 1.0 but do match the paper / matlab
+        # code.
+        weights = np.array(weights if weights else
+                           [0.0448, 0.2856, 0.3001, 0.2363, 0.1333])
+        levels = weights.size
+        downsample_filter = np.ones((2, 2, 1)) / 4.0
+        im1, im2 = [x.astype(np.float64) for x in [original_array, reconstructed_array]]
+        mssim = np.array([])
+        mcs = np.array([])
+        for _ in range(levels):
+            ssim, cs = self._SSIMForMultiScale(
+                im1, im2, max_val=max_val, filter_size=filter_size,
+                filter_sigma=filter_sigma, k1=k1, k2=k2)
+            mssim = np.append(mssim, ssim)
+            mcs = np.append(mcs, cs)
+            filtered = [convolve(im, downsample_filter, mode='reflect')
+                        for im in [im1, im2]]
+            im1, im2 = [x[::2, ::2, :] for x in filtered]
+
+        row[_column_name] = (np.prod(mcs[0:levels - 1] ** weights[0:levels - 1]) *
+                (mssim[levels - 1] ** weights[levels - 1]))
+
+    def _SSIMForMultiScale(self, img1, img2, max_val=255, filter_size=11,
+                           filter_sigma=1.5, k1=0.01, k2=0.03):
+        if img1.shape != img2.shape:
+            raise RuntimeError('Input images must have the same shape (%s vs. %s).',
+                               img1.shape, img2.shape)
+        if img1.ndim != 3:
+            raise RuntimeError('Input images must have four dimensions, not %d',
+                               img1.ndim)
+        img1 = img1.astype(np.float64)
+        img2 = img2.astype(np.float64)
+        height, width, bytes = img1.shape
+
+        # Filter size can't be larger than height or width of images.
+        size = min(filter_size, height, width)
+
+        # Scale down sigma if a smaller filter size is used.
+        sigma = size * filter_sigma / filter_size if filter_size else 0
+
+        if filter_size:
+            window = np.reshape(_FSpecialGauss(size, sigma), (size, size, 1))
+            mu1 = signal.fftconvolve(img1, window, mode='valid')
+            mu2 = signal.fftconvolve(img2, window, mode='valid')
+            sigma11 = signal.fftconvolve(img1 * img1, window, mode='valid')
+            sigma22 = signal.fftconvolve(img2 * img2, window, mode='valid')
+            sigma12 = signal.fftconvolve(img1 * img2, window, mode='valid')
+        else:
+            # Empty blur kernel so no need to convolve.
+            mu1, mu2 = img1, img2
+            sigma11 = img1 * img1
+            sigma22 = img2 * img2
+            sigma12 = img1 * img2
+
+        mu11 = mu1 * mu1
+        mu22 = mu2 * mu2
+        mu12 = mu1 * mu2
+        sigma11 -= mu11
+        sigma22 -= mu22
+        sigma12 -= mu12
+
+        # Calculate intermediate values used by both ssim and cs_map.
+        c1 = (k1 * max_val) ** 2
+        c2 = (k2 * max_val) ** 2
+        v1 = 2.0 * sigma12 + c2
+        v2 = sigma11 + sigma22 + c2
+        ssim = np.mean((((2.0 * mu12 + c1) * v1) / ((mu11 + mu22 + c1) * v2)))
+        cs = np.mean(v1 / v2)
+        return ssim, cs
+
+def _FSpecialGauss(size, sigma):
+    """Function to mimic the 'fspecial' gaussian MATLAB function."""
+    radius = size // 2
+    offset = 0.0
+    start, stop = -radius, radius + 1
+    if size % 2 == 0:
+        offset = 0.5
+        stop -= 1
+    x, y = np.mgrid[offset + start:stop, offset + start:stop]
+    assert len(x) == size
+    g = np.exp(-((x ** 2 + y ** 2) / (2.0 * sigma ** 2)))
+    return g / g.sum()
 
 class SpectralAngleTable(LossyCompressionExperiment):
     """Lossy compression experiment that computes spectral angle "distance" 
