6311e617d5a2237809facf8e0c1aa9d9b8166094
nataliablasco00 <natalia.blascoa@e-campus.uab.cat>
2021-03-17 16:54:04 +0100

    minor fixes HEVC

--- plugins/test_all_codecs/test_all_codecs.py
+++ plugins/test_all_codecs/test_all_codecs.py
@@ -138,12 +138,9 @@ if __name__ == '__main__':
         all_families.append(kakadu_mct_family)
 
     hevc_family = enb.aanalysis.TaskFamily(label="HEVC")
-    c1 = plugin_hevc.hevc_codec.HEVC_lossless()
-    c2 = plugin_hevc.hevc_codec.HEVC_lossy(qp=25)
-    all_codecs.append(c1)
-    all_codecs.append(c2)
-    hevc_family.add_task(c1.name, c1.label)
-    hevc_family.add_task(c2.name, c2.label)
+    for c in (plugin_hevc.hevc_codec.HEVC_lossless(), plugin_hevc.hevc_codec.HEVC_lossy()):
+        all_codecs.append(c)
+        hevc_family.add_task(c.name, c.label)
     all_families.append(hevc_family)
 
     label_by_group_name = dict()
