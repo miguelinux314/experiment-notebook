d79271de396be7872a8e62d4a5e8525712b4b2d5
ester <ester.jara@e-campus.uab.cat>
2021-03-18 19:33:00 +0100

    Added SSIM and MS-SSIM metrics and experiment_ssim.
    Former-commit-id: e7c9ea7e742bd6e2fc2eeacca30c746bcb55f326

--- enb/icompression.py
+++ enb/icompression.py
@@ -20,11 +20,8 @@ import imageio
 import subprocess
 import numpngw
 
-from skimage.transform import resize
 from scipy import signal
 from scipy.ndimage.filters import convolve
-from PIL import Image
-
 
 import enb
 from enb import atable
@@ -886,54 +883,52 @@ class LossyCompressionExperiment(CompressionExperiment):
         row[_column_name] = 10 * math.log10((max_error ** 2) / row["mse"]) \
             if row["mse"] > 0 else float("inf")
 
-class SSIM(CompressionExperiment):
-    @atable.column_function("ms_ssim", label="MS-SSIM", plot_min=0)
-    def set_MultiScaleSSIM(self, index, row):
 
-        max_val = 255
-        filter_size = 11
-        filter_sigma = 1.5
-        k1 = 0.01
-        k2 = 0.03
-        weights = None
+class StructuralSimilarity(CompressionExperiment):
 
+    @atable.column_function([
+        atable.ColumnProperties(name="ssim", label="SSIM", plot_min=0),
+        atable.ColumnProperties(name="ms_ssim", label="MS-SSIM", plot_min=0)])
+    def set_StructuralSimilarity(self, index, row):
         original_array = np.fromfile(row.compression_results.original_path,
                                      dtype=row.numpy_dtype)
         original_array = np.reshape(original_array, (row.image_info_row["width"], row.image_info_row["height"], row.image_info_row["component_count"]))
 
         reconstructed_array = np.fromfile(row.decompression_results.reconstructed_path,
-                                     dtype=row.numpy_dtype)
+                                          dtype=row.numpy_dtype)
         reconstructed_array = np.reshape(reconstructed_array, (row.image_info_row["width"], row.image_info_row["height"], row.image_info_row["component_count"]))
 
-        if original_array.shape != reconstructed_array.shape:
+        row["ssim"] = self.SSIM(original_array, reconstructed_array)
+        row["ms_ssim"] = self.MultiScaleSSIM(original_array, reconstructed_array)
+
+    def MultiScaleSSIM(self, img1, img2, max_val=255, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03, weights=None):
+        if img1.shape != img2.shape:
             raise RuntimeError('Input images must have the same shape (%s vs. %s).',
-                               original_array.shape, reconstructed_array.shape)
-        if original_array.ndim != 3:
+                               img1.shape, img2.shape)
+        if img1.ndim != 3:
             raise RuntimeError('Input images must have four dimensions, not %d',
-                               original_array.ndim)
+                               img1.ndim)
 
         weights = np.array(weights if weights else
                            [0.0448, 0.2856, 0.3001, 0.2363, 0.1333])
         levels = weights.size
         downsample_filter = np.ones((2, 2, 1)) / 4.0
-        im1, im2 = [x.astype(np.float64) for x in [original_array, reconstructed_array]]
+        im1, im2 = [x.astype(np.float64) for x in [img1, img2]]
         mssim = np.array([])
         mcs = np.array([])
         for _ in range(levels):
-            ssim, cs = self._SSIMForMultiScale(
+            ssim, cs = self.SSIM(
                 im1, im2, max_val=max_val, filter_size=filter_size,
-                filter_sigma=filter_sigma, k1=k1, k2=k2)
+                filter_sigma=filter_sigma, k1=k1, k2=k2, full=True)
             mssim = np.append(mssim, ssim)
             mcs = np.append(mcs, cs)
             filtered = [convolve(im, downsample_filter, mode='reflect')
                         for im in [im1, im2]]
             im1, im2 = [x[::2, ::2, :] for x in filtered]
 
-        row[_column_name] = (np.prod(mcs[0:levels - 1] ** weights[0:levels - 1]) *
-                (mssim[levels - 1] ** weights[levels - 1]))
+        return np.prod(mcs[0:levels - 1] ** weights[0:levels - 1]) * (mssim[levels - 1] ** weights[levels - 1])
 
-    def _SSIMForMultiScale(self, img1, img2, max_val=255, filter_size=11,
-                           filter_sigma=1.5, k1=0.01, k2=0.03):
+    def SSIM(self, img1, img2, max_val=255, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03, full=False):
         if img1.shape != img2.shape:
             raise RuntimeError('Input images must have the same shape (%s vs. %s).',
                                img1.shape, img2.shape)
@@ -951,7 +946,7 @@ class SSIM(CompressionExperiment):
         sigma = size * filter_sigma / filter_size if filter_size else 0
 
         if filter_size:
-            window = np.reshape(_FSpecialGauss(size, sigma), (size, size, 1))
+            window = np.reshape(self._FSpecialGauss(size, sigma), (size, size, 1))
             mu1 = signal.fftconvolve(img1, window, mode='valid')
             mu2 = signal.fftconvolve(img2, window, mode='valid')
             sigma11 = signal.fftconvolve(img1 * img1, window, mode='valid')
@@ -978,20 +973,22 @@ class SSIM(CompressionExperiment):
         v2 = sigma11 + sigma22 + c2
         ssim = np.mean((((2.0 * mu12 + c1) * v1) / ((mu11 + mu22 + c1) * v2)))
         cs = np.mean(v1 / v2)
-        return ssim, cs
+        if full:
+            return ssim, cs
+        return ssim
 
-def _FSpecialGauss(size, sigma):
-    """Function to mimic the 'fspecial' gaussian MATLAB function."""
-    radius = size // 2
-    offset = 0.0
-    start, stop = -radius, radius + 1
-    if size % 2 == 0:
-        offset = 0.5
-        stop -= 1
-    x, y = np.mgrid[offset + start:stop, offset + start:stop]
-    assert len(x) == size
-    g = np.exp(-((x ** 2 + y ** 2) / (2.0 * sigma ** 2)))
-    return g / g.sum()
+    def _FSpecialGauss(self, size, sigma):
+        """Function to mimic the 'fspecial' gaussian MATLAB function."""
+        radius = size // 2
+        offset = 0.0
+        start, stop = -radius, radius + 1
+        if size % 2 == 0:
+            offset = 0.5
+            stop -= 1
+        x, y = np.mgrid[offset + start:stop, offset + start:stop]
+        assert len(x) == size
+        g = np.exp(-((x ** 2 + y ** 2) / (2.0 * sigma ** 2)))
+        return g / g.sum()
 
 class SpectralAngleTable(LossyCompressionExperiment):
     """Lossy compression experiment that computes spectral angle "distance" 
