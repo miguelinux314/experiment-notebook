b836991e533beebc41b810fba8e5ecc95a5f154d
nataliablasco00 <natalia.blascoa@e-campus.uab.cat>
2021-03-22 18:21:18 +0100

    Added target bitrate option on HEVC_lossy
    Former-commit-id: 83b99b5771dcdb5b90864fc453fadd512587be6a

--- plugins/plugin_hevc/hevc_codec.py
+++ plugins/plugin_hevc/hevc_codec.py
@@ -12,9 +12,17 @@ options = get_options()
 #       --TargetBitrate          Rate control: target bit-rate
 
 class HEVC(icompression.WrapperCodec):
+    """
+    Base class for HEVC coder
+    """
     def __init__(self, config_path, chroma_format="400", qp=0):
+        """
+        :param chroma_format: Specifies the chroma format used in the input file (only 400 supported).
+        :param qp: Specifies the base value of the quantization parameter (0-51).
+        """
         chroma_format = str(chroma_format)
         assert chroma_format in ["400"], f"Chroma format {chroma_format} not supported."
+
         param_dict = dict(chroma_format=chroma_format)
 
         assert 0 <= qp <= 51
@@ -46,7 +54,13 @@ class HEVC(icompression.WrapperCodec):
 
 
 class HEVC_lossless(icompression.LosslessCodec, HEVC):
+    """
+    HEVC subclass for lossless compression
+    """
     def __init__(self, config_path=None, chroma_format="400"):
+        """
+        :param chroma_format: Specifies the chroma format used in the input file (only 400 supported).
+        """
         config_path = config_path if config_path is not None \
             else os.path.join(os.path.dirname(os.path.abspath(__file__)),
                               f"hevc_lossless_{chroma_format}.cfg")
@@ -58,12 +72,22 @@ class HEVC_lossless(icompression.LosslessCodec, HEVC):
 
 
 class HEVC_lossy(icompression.LossyCodec, HEVC):
-    def __init__(self, config_path=None, chroma_format="400", qp=4):
+    """
+    HEVC subclass for lossy compression
+    """
+    def __init__(self, config_path=None, chroma_format="400", qp=4, bit_rate=None):
+        """
+        :param chroma_format: Specifies the chroma format used in the input file (only 400 supported).
+        :param qp: Specifies the base value of the quantization parameter (0-51).
+        :bit_rate: target bitrate in bps.
+        """
         config_path = config_path if config_path is not None \
             else os.path.join(os.path.dirname(os.path.abspath(__file__)),
                               f"hevc_lossy_{chroma_format}.cfg")
         HEVC.__init__(
             self, config_path=config_path, chroma_format=chroma_format, qp=qp)
+
+        self.param_dict['bit_rate'] = bit_rate
 
     def get_compression_params(self, original_path, compressed_path, original_file_info):
         if original_file_info['bytes_per_sample'] > 1:
@@ -75,8 +99,12 @@ class HEVC_lossy(icompression.LossyCodec, HEVC):
                 compressed_path=compressed_path,
                 original_file_info=original_file_info) + \
                    f" -q {self.param_dict['QP']}"
+
+            if self.param_dict['bit_rate'] is not None:
+                params += f" --RateControl" \
+                          f" --TargetBitrate={self.param_dict['bit_rate']}"
+
             return params
-            
 
     def get_decompression_params(self, compressed_path, reconstructed_path, original_file_info):
         if original_file_info['bytes_per_sample'] > 1:
