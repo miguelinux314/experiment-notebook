f443857d132cc013cfd48b20b3c17308e9919936
OscarMaireles <62958848+OscarMaireles@users.noreply.github.com>
2021-04-13 14:17:37 +0200

    Update fits_to_raw.py
    Former-commit-id: 1c03da28cf64771532e7b2b2cd39b3c782161d80

--- plugins/plugin_fits/fits_to_raw.py
+++ plugins/plugin_fits/fits_to_raw.py
@@ -12,11 +12,13 @@ import enb.isets as isets
 from enb.config import options
 
 
+
 class FitsVersionTable(sets.FileVersionTable, sets.FilePropertiesTable):
     """Read FITS files and convert them to raw files,
     sorting them by type (integer or float)	and by bits per pixel
     """
-    fits_extension = "fits"
+    fits_extension = "fit"
+    allowed_extensions = ["fit", "fits"]
     version_name = "FitsToRaw"
 
     def __init__(self, original_base_dir, version_base_dir):
@@ -27,19 +29,22 @@ class FitsVersionTable(sets.FileVersionTable, sets.FilePropertiesTable):
             version_name=self.version_name)
 
     def get_default_target_indices(self):
-        return sets.get_all_test_files(
-            ext="fit", base_dataset_dir=self.original_base_dir) \
-               + sets.get_all_test_files(
-            ext="fits", base_dataset_dir=self.original_base_dir)
+        indices = []
+        for ext in self.allowed_extensions:
+            indices.extend(sets.get_all_test_files(
+            ext=ext, base_dataset_dir=self.original_base_dir))
+        return indices
 
 
     def original_to_versioned_path(self, original_path):
 
         hdul = fits.open(original_path)
-        header = hdul[0].header 
-        if header['NAXIS'] == 0 :
-            header=hdul[1].header
-
+        hdul_index=0
+        header = hdul[hdul_index].header 
+        while hdul[hdul_index].header["NAXIS"] == 0:
+            hdul_index += 1
+        header = hdul[hdul_index].header
+        
         if header['NAXIS'] == 2:
             if header['BITPIX'] < 0:
                 name_label = f'-f{header["BITPIX"] * -1}-1x{header["NAXIS2"]}x{header["NAXIS1"]}'
@@ -49,12 +54,19 @@ class FitsVersionTable(sets.FileVersionTable, sets.FilePropertiesTable):
                 type_name = f'uint{header["BITPIX"]}'
         elif header['NAXIS'] == 3:
             if header['BITPIX'] < 0:
-                name_label = f'-f{header["BITPIX"] * -1}-{header["NAXIS3"]}x{header["NAXIS2"]}x{header["NAXIS1"]}'
+                name_label = f'-f{header["BITPIX"] * -1}-{header["NAXIS2"]}x{header["NAXIS2"]}x{header["NAXIS1"]}'
                 type_name = f'float{header["BITPIX"] * -1}'
             if header['BITPIX'] > 0:
                 name_label = f'-i{header["BITPIX"]}-{header["NAXIS3"]}x{header["NAXIS2"]}x{header["NAXIS1"]}'
                 type_name = f'uint{header["BITPIX"]}'
-
+        else:
+            raise Exception(f"Invalid header['NAXIS'] = {header['NAXIS']}")
+            
+        if original_path.lower().endswith(".fit"):            
+            input_ext = "fit"
+        elif original_path.lower().endswith(".fits"):
+            input_ext = "fits"
+     
         return os.path.join(
             os.path.dirname(
                 os.path.abspath(original_path)).replace(
@@ -62,21 +74,23 @@ class FitsVersionTable(sets.FileVersionTable, sets.FilePropertiesTable):
                 os.path.abspath(self.version_base_dir)),
             type_name,
             os.path.basename(original_path).replace(
-                f".{self.fits_extension}", f"{name_label}.raw"))
+                f".{input_ext}", f"{name_label}.raw"))
 
     def version(self, input_path, output_path, row):
-        assert input_path.endswith(self.fits_extension), \
-            f"Invalid extension in {input_path} (expected {self.fits_extension})"
+        assert any(input_path.endswith(f".{ext}") for ext in self.allowed_extensions)
 
         hdul = fits.open(input_path)
-        header = hdul[0].header 
-        if header['NAXIS'] == 0 :
-            header=hdul[1].header
+        hdul_index = 0
+        while hdul[hdul_index].header["NAXIS"] == 0:
+            hdul_index += 1
+        header = hdul[hdul_index].header 
         data = fitsio.read(input_path)
         if header['NAXIS'] == 2:
             data = np.expand_dims(data, axis=2)
         elif header['NAXIS'] == 3:
             pass
+        else:
+            raise Exception(f"Unsupported NAXIS {header['NAXIS']} for {input_path}")
 
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
         isets.dump_array_bsq(data, output_path)
