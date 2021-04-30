b337ad1c59b2fd74da27af7fe705ef3a0af16ab1
llabres <artur.llabres@e-campus.uab.cat>
2021-04-30 18:01:55 +0200

    Machine Learning Experiment
    Former-commit-id: 1822f883f4d1d09149c99e7e44253acdb7bae75a

--- enb/ml.py
+++ enb/ml.py
@@ -1,3 +1,4 @@
+import os
 import time
 import torch
 import torch.nn as nn
@@ -5,7 +6,10 @@ import numpy as np
 
 import enb
 from enb import experiment
+from enb.config import get_options
+from enb.atable import indices_to_internal_loc
 
+options = get_options()
 
 class Model(experiment.ExperimentTask):
     def __init__(self, criterion=nn.CrossEntropyLoss(), param_dict=None):
@@ -19,7 +23,7 @@ class Model(experiment.ExperimentTask):
         totals = 0
         all_preds = []
         all_targets = []
-
+        print("HELLO")
         with torch.no_grad():
             for data, target in test_loader:
                 data, target = data, target
@@ -59,6 +63,7 @@ class MachineLearningExperiment(experiment.Experiment):
         def testing_results(self):
             """Perform the actual testing experiment for the selected row.
             """
+            print("HELLO")
             if self._testing_results is None:
                 time_before = time.time()
                 self._testing_results = self.model.test(self.testing_dataset_path)
@@ -77,5 +82,72 @@ class MachineLearningExperiment(experiment.Experiment):
 
         def __contains__(self, item):
             return item in self.row
-    def __init__(self, models):
-        self.models = models
+
+    def __init__(self, models,
+                 dataset_paths=None,
+                 csv_experiment_path=None,
+                 csv_dataset_path=None,
+                 dataset_info_table=None,
+                 overwrite_file_properties=False,
+                 parallel_dataset_property_processing=None):
+        """
+        :param codecs: list of :py:class:`AbstractCodec` instances. Note that
+          codecs are compatible with the interface of :py:class:`ExperimentTask`.
+        :param dataset_paths: list of paths to the files to be used as input for compression.
+          If it is None, this list is obtained automatically from the configured
+          base dataset dir.
+        :param csv_experiment_path: if not None, path to the CSV file giving persistence
+          support to this experiment.
+          If None, it is automatically determined within options.persistence_dir.
+        :param csv_dataset_path: if not None, path to the CSV file given persistence
+          support to the dataset file properties.
+          If None, it is automatically determined within options.persistence_dir.
+        :param dataset_info_table: if not None, it must be a ImagePropertiesTable instance or
+          subclass instance that can be used to obtain dataset file metainformation,
+          and/or gather it from csv_dataset_path. If None, a new ImagePropertiesTable
+          instance is created and used for this purpose.
+        :param overwrite_file_properties: if True, file properties are recomputed before starting
+          the experiment. Useful for temporary and/or random datasets. Note that overwrite
+          control for the experiment results themselves is controlled in the call
+          to get_df
+        :param parallel_dataset_property_processing: if not None, it determines whether file properties
+          are to be obtained in parallel. If None, it is given by not options.sequential.
+        """
+        table_class = type(dataset_info_table) if dataset_info_table is not None \
+            else self.default_file_properties_table_class
+        csv_dataset_path = csv_dataset_path if csv_dataset_path is not None \
+            else os.path.join(options.persistence_dir, f"{table_class.__name__}_persistence.csv")
+        imageinfo_table = dataset_info_table if dataset_info_table is not None \
+            else table_class(csv_support_path=csv_dataset_path)
+
+        csv_dataset_path = csv_dataset_path if csv_dataset_path is not None \
+            else f"{dataset_info_table.__class__.__name__}_persistence.csv"
+        super().__init__(tasks=models,
+                         dataset_paths=dataset_paths,
+                         csv_experiment_path=csv_experiment_path,
+                         csv_dataset_path=csv_dataset_path,
+                         dataset_info_table=imageinfo_table,
+                         overwrite_file_properties=overwrite_file_properties,
+                         parallel_dataset_property_processing=parallel_dataset_property_processing)
+
+    @property
+    def models_by_name(self):
+        """Alias for :py:attr:`tasks_by_name`
+        """
+        return self.tasks_by_name
+
+    def process_row(self, index, column_fun_tuples, row, overwrite, fill):
+        # Right now we are using file_path as testing_dataset_path maybe we will need to also add training_dataset_path
+        file_path, model_name = index
+        model = self.models_by_name[model_name]
+        image_info_row = self.dataset_table_df.loc[indices_to_internal_loc(file_path)]
+        row_wrapper = self.RowWrapper(file_path, model, row)
+        result = super().process_row(index=index, column_fun_tuples=column_fun_tuples,
+                                     row=row_wrapper, overwrite=overwrite, fill=fill)
+
+
+        if isinstance(result, Exception):
+            return result
+
+        print(result.__dict__)
+        return row
