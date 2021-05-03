165cd6598d0b0a8c305bfeb4887aa32e45713b90
llabres <artur.llabres@e-campus.uab.cat>
2021-05-03 17:54:37 +0200

    Machine Learning Experiment
    Former-commit-id: 76d983c57889359de6e2742530c31854bc75193b

--- templates/ML_experiment/ml_experiment.py
+++ templates/ML_experiment/ml_experiment.py
@@ -1,3 +1,5 @@
+import torchvision
+
 from enb.config import get_options
 
 from enb import ml
@@ -10,7 +12,10 @@ if __name__ == '__main__':
     models = []
     models.append(model_Resnet18.model_resnet18.Resnet18(2))
 
-    exp = ml.MachineLearningExperiment(models=models)
+    dataset_path = './datasets/'
+    torchvision.datasets.FashionMNIST(dataset_path, train=False, download=True)
+
+    exp = ml.MachineLearningExperiment(models=models, dataset_paths=dataset_path)
 
     df = exp.get_df(parallel_row_processing=not options.sequential,
                     overwrite=options.force > 0)
