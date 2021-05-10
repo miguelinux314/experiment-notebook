03438a15534d7115ce9c3c7884a315f0732bac0b
OscarMaireles <62958848+OscarMaireles@users.noreply.github.com>
2021-05-10 14:06:41 +0200

    Update raw_to_fits.py
    Former-commit-id: f18c26194f327db59deafc44a8880195e4ba0202

--- plugins/plugin_fits/raw_to_fits.py
+++ plugins/plugin_fits/raw_to_fits.py
@@ -7,65 +7,39 @@ import enb.sets import sets
 import enb.isets as isets
 from enb.config import options
 
-class FitsToRaw(sets.FileVersionTable, sets.FilePropertiesTable):
+class RawToFits(sets.FileVersionTable, sets.FilePropertiesTable):
     def __init__(self, original_base_dir, version_base_dir):
         super().__init__(
             original_base_dir=original_base_dir,
             version_base_dir=version_base_dir,
             original_properties_table=sets.FilePropertiesTable(),
-            version_name=self.version_name)
-            
+            version_name=self.version_name)    
             
     def Version(input_path):
-        FITS=glob.glob('./fits_data/*.fit*')
-
-        for i in range(len(FITS)):
-            hdul = fits.open(FITS[i])
-            print(FITS[i])
-            hdul_index=0
-            header = hdul[hdul_index].header 
-            while hdul[hdul_index].header["NAXIS"] == 0:
-                hdul_index += 1  
-    
-            if FITS[i].lower().endswith(".fit"):            
-                input_ext = ".fit"
-                dir_name=FITS[i].replace('./fits_data/','')
-                file_name=dir_name.replace(input_ext,'')
-            elif FITS[i].lower().endswith(".fits"):
-                input_ext = ".fits"   
-                dir_name=FITS[i].replace('./fits_data/','')
-                file_name=dir_name.replace(input_ext,'')
-            
-            for j in range(len(hdul)-hdul_index):        
-                data=hdul[hdul_index].data.transpose()
-                header = hdul[hdul_index].header
-                if header['NAXIS'] == 2:
-                    if header['BITPIX'] < 0:
-                        name_label = f'-float{header["BITPIX"] * -1}-1x{header["NAXIS2"]}x{header["NAXIS1"]}'
-                        type_name = f'float{header["BITPIX"] * -1}'
-                    if header['BITPIX'] > 0:
-                        name_label = f'-uint{header["BITPIX"]}-1x{header["NAXIS2"]}x{header["NAXIS1"]}'
-                        type_name = f'uint{header["BITPIX"]}'
-                    data = np.expand_dims(data, axis=2)
-                elif header['NAXIS'] == 3:
-                    if header['BITPIX'] < 0:
-                        name_label = f'-float{header["BITPIX"] * -1}-{header["NAXIS3"]}x{header["NAXIS2"]}x{header["NAXIS1"]}'
-                        type_name = f'float{header["BITPIX"] * -1}'
-                    if header['BITPIX'] > 0:
-                        name_label = f'-uint{header["BITPIX"]}-{header["NAXIS3"]}x{header["NAXIS2"]}x{header["NAXIS1"]}'
-                        type_name = f'uint{header["BITPIX"]}'                
-                else:
-                    raise Exception(f"Invalid header['NAXIS'] = {header['NAXIS']}")
+        RAWS=glob.glob('./raw_data/*.raw')
         
-                output_folder=f'./raw_data/{type_name}/'
-                os.makedirs(os.path.dirname(f'{output_folder}'), exist_ok=True)
-                data=data.swapaxes(0,2)
-                data.astype(type_name).tofile(f'{output_folder}/{file_name}-extension{j}-{name_label}.raw')
-                header.totextfile(f'{output_folder}/{file_name}-extension{j}-{name_label}.txt')
-                hdul_index += 1
+        for i in range(len(RAWS)):
+            file=RAWS[i]
+            params = file[7:-4].split("-")
+            dimensions=params[-1].split('x')
+            print(params)
+            print(dimensions)
+            frames=int(dimensions[-3])
+            columns=int(dimensions[-2])
+            rows=int(dimensions[-1])
+            astype=params[-2]
+            name=params[0]
+            extension=params[-4]
+            
+            img=np.fromfile(open(file), dtype = f'{astype}',  count = -1)
+            print(img.shape)
+            array=np.reshape(img,(frames,columns,rows))
 
+            hdu = fits.PrimaryHDU(array, header=Header.fromfile(f'{file[0:-4]}.txt',sep='\n', endcard=False,padding=False))
+            hdu.writeto(f'./fits_data/{name[0:-2]}_{extension}.fits')
+            
 if __name__ == '__main__':
-    print("This example converts all .fit files in fits_data into raw_data, preserving "
-          "the directory hierarchy.")
-    input_path='./fits_data'
-    FitsToRaw.Version(input_path)
+    print("This example converts all raw files in raw_data into fits_data, preserving "
+          "the original fits header")
+    input_path='./raw_data'
+    RawToFits.Version(input_path)
