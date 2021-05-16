ab76dca6e9c3c8247ad31d618699aef739674861
llabres <artur.llabres@e-campus.uab.cat>
2021-05-16 18:27:27 +0200

    Work in progress Machine Learning Experiment

--- templates/ML_experiment/ml_experiment.py
+++ templates/ML_experiment/ml_experiment.py
@@ -1,4 +1,4 @@
-import torchvision
+from torchvision import datasets, transforms
 
 from enb.config import get_options
 
@@ -10,12 +10,16 @@ options = get_options(from_main=False)
 
 if __name__ == '__main__':
     models = []
-    models.append(model_Resnet18.model_resnet18.Resnet18(2))
+    models.append(model_Resnet18.model_resnet18.Resnet18(10))
 
     dataset_path = './datasets/'
-    torchvision.datasets.FashionMNIST(dataset_path, train=False, download=True)
+    test_set = datasets.FashionMNIST("data", train=False, download=True,
+                                     transform=transforms.Compose([
+                                         transforms.Lambda(lambda image: image.convert('RGB')),
+                                         transforms.ToTensor(),
+                                         transforms.Resize((224, 224))]))
 
-    exp = ml.MachineLearningExperiment(models=models, dataset_paths=dataset_path)
+    exp = ml.MachineLearningExperiment(models=models, dataset_paths=dataset_path, test_set=test_set)
 
     df = exp.get_df(parallel_row_processing=not options.sequential,
                     overwrite=options.force > 0)
