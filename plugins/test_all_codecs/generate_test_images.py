1d8f4af40515e5acd2239dad5c5e8af495afc74a
OscarMaireles <62958848+OscarMaireles@users.noreply.github.com>
2021-06-30 16:18:36 +0200

    Update generate_test_images.py
    Former-commit-id: a1d205fd43a57ec1926eb505a97e825ff7940c87

--- plugins/test_all_codecs/generate_test_images.py
+++ plugins/test_all_codecs/generate_test_images.py
@@ -49,40 +49,6 @@ if __name__ == '__main__':
 
                     with open(output_path, "wb") as output_file:
                         output_file.write(bytes(samples.astype(dtype)))
-                        
-            for bytes_per_sample in [2,4]:
-                if signed and bytes_per_sample == 1:
-                    continue
-
-                for missing_bits in range(8):
-                    bits_per_sample = 8 * bytes_per_sample - missing_bits
-
-                    output_dir = os.path.join(base_dir, f"{label}_{'s' if signed else 'f'}{bits_per_sample}be")
-                    shutil.rmtree(output_dir, ignore_errors=True)
-                    os.makedirs(output_dir)
-                    
-                    type = f"{'s' if signed else 'f'}{8 * bytes_per_sample}be"
-                    dtype = f"{'>i' if signed else 'f'}{bytes_per_sample}"
-                    
-                    geometry = f"{component_count}x{height}x{width}"
-                    output_path = os.path.join(output_dir, f"sample_{type}-{geometry}.raw")
-
-                    total_samples = width*height*component_count
-                    if signed:
-                        min_sample_value = - (2 ** (bits_per_sample-1))
-                        max_sample_value = 2 ** (bits_per_sample-1) - 1
-                    else:
-                        min_sample_value = np.finfo('f').min
-                        max_sample_value = np.finfo('f').max
-                    samples = np.zeros(total_samples)
-                    for i in range(total_samples):
-                        samples[i] = min_sample_value + i % (max_sample_value - min_sample_value + 1)
-                    samples[0] = min_sample_value
-                    samples[1] = max_sample_value
-
-
-                    with open(output_path, "wb") as output_file:
-                        output_file.write(bytes(samples.astype(dtype)))
 
             for bytes_per_sample in [2,4,8]:
                 if signed and bytes_per_sample == 1:
@@ -91,23 +57,23 @@ if __name__ == '__main__':
                 for missing_bits in range(8):
                     bits_per_sample = 8 * bytes_per_sample - missing_bits
 
-                    output_dir = os.path.join(base_dir, f"{label}_{'s' if signed else 'u' and 'f'}{bits_per_sample}be")
+                    output_dir = os.path.join(base_dir, f"{label}_{'f'}{bits_per_sample}be")
                     shutil.rmtree(output_dir, ignore_errors=True)
                     os.makedirs(output_dir)
                     
-                    type = f"{'s' if signed else 'u' and 'f'}{8 * bytes_per_sample}be"
-                    dtype = f"{'>i' if signed else '>u' and 'f'}{bytes_per_sample}"
+                    type = f"{'f'}{8 * bytes_per_sample}be"
+                    dtype = f"{'f'}{bytes_per_sample}"
                     
                     geometry = f"{component_count}x{height}x{width}"
                     output_path = os.path.join(output_dir, f"sample_{type}-{geometry}.raw")
 
                     total_samples = width*height*component_count
                     if signed:
-                        min_sample_value = - (2 ** (bits_per_sample-1))
-                        max_sample_value = 2 ** (bits_per_sample-1) - 1
+                        min_sample_value = np.finfo('f').min
+                        max_sample_value = np.finfo('f').max
                     else:
-                        min_sample_value = 0
-                        max_sample_value = 2**bits_per_sample - 1
+                        min_sample_value = np.finfo('f').min
+                        max_sample_value = np.finfo('f').max
                     samples = np.zeros(total_samples)
                     for i in range(total_samples):
                         samples[i] = min_sample_value + i % (max_sample_value - min_sample_value + 1)
