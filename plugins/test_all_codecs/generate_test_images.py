62f561bb5c7554ac6341fdba85fbab869a19f78f
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2021-07-06 20:26:09 +0200

    Updated and completed sample generation script

--- plugins/test_all_codecs/generate_test_images.py
+++ plugins/test_all_codecs/generate_test_images.py
@@ -16,66 +16,73 @@ if __name__ == '__main__':
     base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
     for label, component_count in [("mono", 1), ("rgb", 3), ("rgba", 4), ("multi", 10)]:
-        for signed in [True, False]:
-            for is_float in [True, False]:
-                for bytes_per_sample in ([2, 4, 8] if is_float else [1, 2, 4]):
+        for bytes_per_sample in [1, 2, 4, 8]:
+            for signed in [True, False]:
+                for big_endian in [True, False]:
+                    for is_float in [True, False]:
+                        if (is_float and bytes_per_sample < 2) \
+                                or (not is_float and bytes_per_sample > 4) \
+                                or (is_float and not signed) \
+                                or (is_float and not big_endian) \
+                                or (signed and bytes_per_sample < 2) \
+                                or (not big_endian and bytes_per_sample <= 1):
+                            continue
 
-                    if is_float:
-                        # Float  samples
-                        bits_per_sample = 8 * bytes_per_sample
+                        if is_float:
+                            # Float  samples
+                            effective_bits_per_sample = 8 * bytes_per_sample
 
-                        output_dir = os.path.join(base_dir, f"{label}_f{bits_per_sample}")
-                        shutil.rmtree(output_dir, ignore_errors=True)
-                        os.makedirs(output_dir)
+                            output_dir = os.path.join(
+                                base_dir,
+                                f"{label}_float",
+                                f"{label}_f{effective_bits_per_sample}")
+                            shutil.rmtree(output_dir, ignore_errors=True)
+                            os.makedirs(output_dir)
 
-                        type = f"f{bits_per_sample}"
-                        dtype = f"<f{bytes_per_sample}"
-                        geometry = f"{component_count}x{height}x{width}"
-                        output_path = os.path.join(output_dir, f"sample_{type}-{geometry}.raw")
-                        total_samples = width * height * component_count
+                            type = f"f{effective_bits_per_sample}"
+                            dtype = f"f{bytes_per_sample}"
+                            geometry = f"{component_count}x{height}x{width}"
+                            output_path = os.path.join(output_dir, f"testsample_{type}-{geometry}.raw")
+                            total_samples = width * height * component_count
 
-                        if bytes_per_sample == 2:
-                            min_sample_value = np.finfo(np.float16).min
-                            max_sample_value = np.finfo(np.float16).max
-                        elif bytes_per_sample == 4:
-                            min_sample_value = np.finfo(np.float32).min
-                            max_sample_value = np.finfo(np.float32).max
-                        elif bytes_per_sample == 8:
-                            min_sample_value = np.finfo(np.float64).min
-                            max_sample_value = np.finfo(np.float64).max
+                            if bytes_per_sample == 2:
+                                min_sample_value = np.finfo(np.float16).min
+                                max_sample_value = np.finfo(np.float16).max
+                            elif bytes_per_sample == 4:
+                                min_sample_value = np.finfo(np.float32).min
+                                max_sample_value = np.finfo(np.float32).max
+                            elif bytes_per_sample == 8:
+                                min_sample_value = np.finfo(np.float64).min
+                                max_sample_value = np.finfo(np.float64).max
 
-                        samples = max_sample_value * np.random.rand(total_samples).astype(np.float64)
-                        samples[::2] *= -1
-                        samples[0] = min_sample_value
-                        samples[1] = max_sample_value
-                        with open(output_path, "wb") as output_file:
-                            output_file.write(bytes(samples.astype(dtype)))
+                            samples = max_sample_value * np.random.rand(total_samples).astype(np.float64)
+                            samples[::2] *= -1
+                            samples[0] = min_sample_value
+                            samples[1] = max_sample_value
+                            with open(output_path, "wb") as output_file:
+                                output_file.write(bytes(samples.astype(dtype)))
 
-                    else:
-                        for big_endian in ([True, False] if bytes_per_sample > 1 else [True]):
+                        else:
                             # Integer samples
-                            if signed and bytes_per_sample == 1:
-                                continue
+                            type = f"{'s' if signed else 'u'}{8 * bytes_per_sample}{'be' if big_endian else 'le'}"
+                            dtype = f"{'>' if big_endian else '<'}{'i' if signed else 'u'}{bytes_per_sample}"
+                            geometry = f"{component_count}x{height}x{width}"
+                            output_dir = os.path.join(
+                                base_dir, f"{label}_{type}")
+                            shutil.rmtree(output_dir, ignore_errors=True)
+                            os.makedirs(output_dir)
 
-                            for missing_bits in range(8):
-                                bits_per_sample = 8 * bytes_per_sample - missing_bits
-                                type = f"{'s' if signed else 'u'}{8 * bytes_per_sample}{'be' if big_endian else 'le'}"
-                                dtype = f"{'>' if big_endian else '<'}{'i' if signed else 'u'}{bytes_per_sample}"
-                                geometry = f"{component_count}x{height}x{width}"
+                            for missing_bits in range(8 * bytes_per_sample):
+                                effective_bits_per_sample = 8 * bytes_per_sample - missing_bits
+                                output_path = os.path.join(output_dir, f"testsample_effective_dr_{effective_bits_per_sample}bit_{type}-{geometry}.raw")
 
-                                output_dir = os.path.join(base_dir, f"{label}_{type}")
-                                shutil.rmtree(output_dir, ignore_errors=True)
-                                os.makedirs(output_dir)
-
-                                output_path = os.path.join(output_dir, f"sample_{type}-{geometry}.raw")
-
-                                total_samples = width*height*component_count
+                                total_samples = width * height * component_count
                                 if signed:
-                                    min_sample_value = - (2 ** (bits_per_sample-1))
-                                    max_sample_value = 2 ** (bits_per_sample-1) - 1
+                                    min_sample_value = - (2 ** (effective_bits_per_sample - 1))
+                                    max_sample_value = 2 ** (effective_bits_per_sample - 1) - 1
                                 else:
                                     min_sample_value = 0
-                                    max_sample_value = 2**bits_per_sample - 1
+                                    max_sample_value = 2 ** effective_bits_per_sample - 1
                                 samples = np.zeros(total_samples)
                                 for i in range(total_samples):
                                     samples[i] = min_sample_value + i % (max_sample_value - min_sample_value + 1)
@@ -83,7 +90,3 @@ if __name__ == '__main__':
                                 samples[1] = max_sample_value
                                 with open(output_path, "wb") as output_file:
                                     output_file.write(bytes(samples.astype(dtype)))
-
-
-
-
