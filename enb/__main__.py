ce01fc164569be5a9a9c079309a636563e2b454b
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2021-07-11 17:36:57 +0200

    Revamped the global option configuration system

--- enb/__main__.py
+++ enb/__main__.py
@@ -6,3 +6,6 @@ import enb
 def enb():
     print(f"Welcome to enb {enb.__version__}, your Experiment Notebook framework.")
     print(f"A command-line interface will be made available here soon.")
+
+def return_banner():
+    return f"enb - Experiment Notebook :: {enb.__version__}"
