44df3d8464d35985d0bf74b7ad64d952a892e8bf
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2021-07-12 10:59:35 +0200

    Minimal changes to the entry point to have a clean slate onto which to work the CLI methods
    Former-commit-id: 3017d73df4df33d38fdb0b75a9db270119faf451

--- enb/__main__.py
+++ enb/__main__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Entropy point for the enb CLI.
 """
-import enb
 
-def enb():
-    print(f"Welcome to enb {enb.__version__}, your Experiment Notebook framework.")
+
+def main():
+    print(f"\n{return_banner()}\n")
+    print(f"Welcome to enb, your Experiment Notebook framework.\n")
     print(f"A command-line interface will be made available here soon.")
 
+
 def return_banner():
-    return f"enb - Experiment Notebook :: {enb.__version__}"
+    return f"{' [ enb - Experiment Notebook ] ': ^80}"
