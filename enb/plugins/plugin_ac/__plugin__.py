c9b67f39cb259e7a37e8c1acf64f5466c5a14aab
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2022-02-26 16:32:07 +0100

    Added arithmetic codec plugin

---
+++ enb/plugins/plugin_ac/__plugin__.py
@@ -0,0 +1,14 @@
+import enb.plugins
+
+
+class ACPlugin(enb.plugins.PluginMake):
+    name = "arithmetic-coder"
+    label = "Arithmetic coder (8 bit)"
+    tags = {"data compression", "codec"}
+    contrib_authors = ["Nayuki"]
+    contrib_reference_urls = ["https://github.com/nayuki/Reference-arithmetic-coding/"]
+    contrib_download_url_name = [
+        (
+        "https://github.com/miguelinux314/experiment-notebook/blob/dev/contrib/nayuki_arithmetic_coder_reference.zip?raw=true",
+        "nayuki_arithmetic_coder_reference.zip")]
+    tested_on = {"linux"}
---
+++ enb/plugins/plugin_ac/arithmetic_codec.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Wrapper for Nayuki's arithmetic coder, originally downloaded from 
+https://github.com/nayuki/Reference-arithmetic-coding/
+"""
+__author__ = "Miguel Hernández-Cabronero"
+__since__ = "2021/06/01"
+
+import os
+from enb import icompression
+
+
+class ArithmeticCoder(icompression.WrapperCodec, icompression.LosslessCodec):
+    """Wrapper for Nayuki's arithmetic coder, originally downloaded from 
+    https://github.com/nayuki/Reference-arithmetic-coding/
+    """
+
+    def __init__(self, compressor_path=None, decompressor_path=None):
+        if compressor_path is None:
+            compressor_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ArithmeticCompress")
+        if decompressor_path is None:
+            decompressor_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ArithmeticDecompress")
+        super().__init__(compressor_path=compressor_path, decompressor_path=decompressor_path)
+
+    def get_compression_params(self, original_path, compressed_path, original_file_info):
+        try:
+            os.remove(compressed_path)
+        except FileNotFoundError:
+            pass
+        return f"{original_path} {compressed_path}"
+
+    def get_decompression_params(self, compressed_path, reconstructed_path, original_file_info):
+        try:
+            os.remove(reconstructed_path)
+        except FileNotFoundError:
+            pass
+        return f"{compressed_path} {reconstructed_path}"
+
+    @property
+    def label(self):
+        return "AC"
