a0f2226a2192d2f8e4342bdeb6648742f8f53338
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2022-02-26 15:57:05 +0100

    Code cleanup in the lz4, flif and fse-huffman codecs
    Former-commit-id: 5a1eda0cd9354e167a40725b73c5f18541da70e8

--- enb/plugins/plugin_fse_huffman/huffman_codec.py
+++ enb/plugins/plugin_fse_huffman/huffman_codec.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Huffman codec wrapper
+"""FSE's Huff0 codec wrapper.
 """
 __author__ = "Òscar Maireles and Miguel Hernández-Cabronero"
 __since__ = "2021/06/01"
@@ -8,7 +8,9 @@ import os
 from enb import icompression
 
 
-class Huffman(icompression.WrapperCodec, icompression.LosslessCodec):
+class FSEHuffman(icompression.WrapperCodec, icompression.LosslessCodec):
+    """FSE's Huff0 codec wrapper.
+    """
     def __init__(self, huff_binary=os.path.join(os.path.dirname(os.path.abspath(__file__)), "fse")):
         icompression.WrapperCodec.__init__(self,
                                            compressor_path=huff_binary,
@@ -20,11 +22,19 @@ class Huffman(icompression.WrapperCodec, icompression.LosslessCodec):
         assert os.path.isfile(huff_binary)
 
     def get_compression_params(self, original_path, compressed_path, original_file_info):
-        return f"-h -f {original_path} {compressed_path}"
+        try:
+            os.remove(compressed_path)
+        except FileNotFoundError:
+            pass
+        return f"-h {original_path} {compressed_path}"
 
     def get_decompression_params(self, compressed_path, reconstructed_path, original_file_info):
-        return f"-h -d -f {compressed_path} {reconstructed_path}"
+        try:
+            os.remove(reconstructed_path)
+        except FileNotFoundError:
+            pass
+        return f"-h -d {compressed_path} {reconstructed_path}"
 
     @property
     def label(self):
-        return "Huffman"
+        return "FSE-HUF"
