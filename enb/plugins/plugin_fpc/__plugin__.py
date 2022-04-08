3d00f5a2545f55a22a787c5f8b8c9066ddd4da12
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2022-04-08 14:52:21 +0200

    Documentation cleanup

--- enb/plugins/plugin_fpc/__plugin__.py
+++ enb/plugins/plugin_fpc/__plugin__.py
@@ -3,7 +3,7 @@ import enb.plugins
 
 class FPCPlugin(enb.plugins.PluginMake):
     name = "fpc"
-    label = "FPC codec wrapers"
+    label = "FPC codec wrappers"
     tags = {"data compression", "codec"}
     contrib_authors = ["Martin Burtscher et al."]
     contrib_reference_urls = ["https://userweb.cs.txstate.edu/~burtscher/research/FPC/"]
