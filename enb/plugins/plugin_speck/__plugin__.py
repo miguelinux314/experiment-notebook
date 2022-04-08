6852aa0941adae73cee450a519b95f319bb1add5
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2022-04-08 14:51:57 +0200

    Added a SPECK codec
    Former-commit-id: fd01106210f8d8e9133b0627e16e5fa491c2d8d4

---
+++ enb/plugins/plugin_speck/__plugin__.py
@@ -0,0 +1,11 @@
+import enb.plugins
+
+
+class SpeckPlugin(enb.plugins.plugin.PluginJava):
+    name = "speck"
+    label = "Wrapper for the SPECK codec"
+    tags = {"data compression", "codec"}
+    contrib_authors = ["Group on Interactive Coding of Images (GICI)"]
+    contrib_reference_urls = [
+        "https://github.com/miguelinux314/experiment-notebook/blob/dev/contrib/SPModule_20220804.zip?raw=true"]
+    tested_on = {"linux"}
