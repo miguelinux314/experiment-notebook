0a6d26fb15667f91e947ed9424793b8f1aab1b5f
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2022-05-30 15:38:15 +0200

    Updated zstandard plugin by Òscar Maireles
    Former-commit-id: 76e87bf51ed7bbd387cb56ad42f6dfb77ac81aa7

--- enb/plugins/plugin_zstandard/__plugin__.py
+++ enb/plugins/plugin_zstandard/__plugin__.py
@@ -4,9 +4,10 @@ class ZstandardPlugin(enb.plugins.PluginMake):
     name = "zstd"
     label = "Zstandard library wrapper"
     tags = {"data compression", "codec"}
+    authors = ["Òscar Maireles"]
     contrib_authors = ["Yann Collet"]
     contrib_reference_urls = ["https://github.com/facebook/zstd"]
     contrib_download_url_name = [
-        ("https://github.com/miguelinux314/experiment-notebook/blob/dev/contrib/zstd-dev.zip?raw=true",
-         "zstd-dev.zip")]
+        ("https://github.com/miguelinux314/experiment-notebook/blob/dev/contrib/zstd-1.1.3.zip?raw=true",
+         "zstd-1.1.3.zip")]
     tested_on = {"linux"}
