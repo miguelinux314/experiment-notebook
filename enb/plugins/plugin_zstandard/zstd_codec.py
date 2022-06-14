60baa1be4c3d33256c21cbfa0a6caf480c301106
omaireles <oscar.maireles@uab.cat>
2022-06-14 17:56:03 +0200

    files updated

--- enb/plugins/plugin_zstandard/zstd_codec.py
+++ enb/plugins/plugin_zstandard/zstd_codec.py
@@ -8,13 +8,8 @@ import os
 import enb
 from enb import tarlite
 import tempfile
-<<<<<<< HEAD
-from enb.config import options
-import glob
-=======
 import subprocess
 
->>>>>>> 4a625ebc9d50110d4d5d2fa2828130b7e38a5fda
 
 class Zstandard(enb.icompression.LosslessCodec, enb.icompression.NearLosslessCodec, enb.icompression.FITSWrapperCodec):
     """Wrapper for the Zstandard codec
@@ -50,52 +45,10 @@ class Zstandard(enb.icompression.LosslessCodec, enb.icompression.NearLosslessCod
         return f"-d  -f {compressed_path} -o {reconstructed_path}"
 
 
-<<<<<<< HEAD
-class Zstandard_train(enb.icompression.LosslessCodec, enb.icompression.NearLosslessCodec, enb.icompression.FITSWrapperCodec):
-    """Wrapper for the Zstandard codec
-    All data types integer and float 16, 32, 64 can be compressed
-    """
-    def __init__(self, compression_level='19', zstd_binary=os.path.join(os.path.dirname(__file__), "zstd")):
-        """
-        :param compression_level: 1-19, being 19 the maximum data reduction
-        """
-        super().__init__(compressor_path=zstd_binary,
-                         decompressor_path=zstd_binary,
-                         param_dict=dict(compression_level=compression_level))
-    '''
-    def get_compression_params(self, original_path, compressed_path, original_file_info):
-        """Return a string (shell style) with the parameters
-        to be passed to the compressor.
-        Same parameter semantics as :meth:`AbstractCodec.compress`.
-        :param original_file_info: a dict-like object describing original_path's properties
-          (e.g., geometry), or None
-        """
-        raise NotImplementedError()
-    def get_decompression_params(self, compressed_path, reconstructed_path, original_file_info):
-        """Return a string (shell style) with the parameters
-        to be passed to the decompressor.
-        Same parameter semantics as :meth:`AbstractCodec.decompress()`.
-        :param original_file_info: a dict-like object describing original_path's properties
-          (e.g., geometry), or None
-        """
-        raise NotImplementedError()
-    '''
-    def compress(self, original_path, compressed_path, original_file_info):
-        print('FILES', original_path, compressed_path)
-        os.system(f" {self.compressor_path} --train {original_path} --optimize-cover -o {os.path.join(os.path.dirname(__file__), 'dict.zstd')}")
-        os.system(f" {self.compressor_path} --ultra -{self.param_dict['compression_level']}  -D {os.path.join(os.path.dirname(__file__), 'dict.zstd')}  -f {original_path}  -o {os.path.join(os.path.dirname(__file__), 'compressed_trained_path.zstd')}")
-        input_paths=[os.path.join(os.path.dirname(__file__), 'dict.zstd'), os.path.join(os.path.dirname(__file__), 'compressed_trained_path.zstd')]
-
-        tarlite.tarlite_files(input_paths=input_paths, output_tarlite_path=compressed_path)
-
-    def decompress(self, compressed_path, reconstructed_path, original_file_info):
-        tr = tarlite.untarlite_files(input_tarlite_path=compressed_path, output_dir_path=os.path.dirname(__file__))
-        os.system(f"{self.compressor_path} -d -D {os.path.join(os.path.dirname(__file__), 'dict.zstd')}  -f {os.path.join(os.path.dirname(__file__), 'compressed_trained_path.zstd')} -o {reconstructed_path}")
-=======
 class Zstandard_train(Zstandard):
     """Wrapper for the Zstandard codec, which produces a dictionary file for the input data and then employs
     it for compression. The generated dictionary is included in the compressed data size measurements.
-    
+
     All data types integer and float 16, 32, 64 can be compressed
     """
 
@@ -108,7 +61,7 @@ class Zstandard_train(Zstandard):
 
     def compress(self, original_path, compressed_path, original_file_info):
         self.assert_valid_data_type(original_file_info=original_file_info)
-        
+
         with tempfile.TemporaryDirectory(dir=enb.config.options.base_tmp_dir) as tmp_dir:
             dict_path = os.path.join(tmp_dir, "dict.zstd")
             dict_compressed_path = os.path.join(tmp_dir, "compressed.zstd")
@@ -151,4 +104,3 @@ class Zstandard_train(Zstandard):
     @property
     def label(self):
         return f"Zstandard pretrain {self.param_dict['compression_level']}"
->>>>>>> 4a625ebc9d50110d4d5d2fa2828130b7e38a5fda
