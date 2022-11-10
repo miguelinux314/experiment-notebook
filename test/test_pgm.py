739036450ecbe1b7ab01e452328d613d6e8c54a9
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2022-11-10 16:01:06 +0100

    Added support for PPM files in the enb.pgm module
    Former-commit-id: d7e445cf5aa11a9a0893a2bd78e3f32d4c9e5843

--- test/test_pgm.py
+++ test/test_pgm.py
@@ -6,34 +6,42 @@ __since__ = "2020/04/07"
 
 import unittest
 import tempfile
-import os
 import numpy as np
 
 import test_all
 from enb import pgm
-from enb import isets
+
+
+def get_test_component(bytes_per_sample, width=67, height=34):
+    array = np.zeros((width, height), dtype=f"u{bytes_per_sample}")
+    max_pixel_value = 2 ** (8 * bytes_per_sample) - 1
+    i = 0
+    for y in range(height):
+        for x in range(width):
+            array[x, y] = (i % (max_pixel_value + 1))
+            i += 1
+    array[0, 0] = max_pixel_value
+    assert array[0, 0] == max_pixel_value
+    return array
 
 
 class TestPGM(unittest.TestCase):
-
     def test_pgm_read_write(self):
-        width = 67
-        height = 34
-
         for bytes_per_sample in [1, 2]:
-            array = np.zeros((width, height), dtype=f"u{bytes_per_sample}")
-            max_pixel_value = 2 ** (8 * bytes_per_sample) - 1
-            i = 0
-            for y in range(height):
-                for x in range(width):
-                    array[x, y] = (i % (max_pixel_value + 1))
-                    i += 1
-            array[0, 0] = max_pixel_value
-            assert array[0, 0] == max_pixel_value
+            array = get_test_component(bytes_per_sample=bytes_per_sample)
 
             with tempfile.NamedTemporaryFile() as tmp_file:
                 pgm.write_pgm(array=array, bytes_per_sample=bytes_per_sample, output_path=tmp_file.name)
                 loaded_array = pgm.read_pgm(input_path=tmp_file.name)
+                assert array.shape == loaded_array.shape
+                assert (array == loaded_array).all(), (array, '\n', loaded_array)
+
+    def test_ppm_read_write(self):
+        for bytes_per_sample in [1, 2]:
+            array = np.dstack([get_test_component(bytes_per_sample=bytes_per_sample)] * 3)
+            with tempfile.NamedTemporaryFile() as tmp_file:
+                pgm.write_ppm(array=array, bytes_per_sample=bytes_per_sample, output_path=tmp_file.name)
+                loaded_array = pgm.read_ppm(input_path=tmp_file.name)
                 assert array.shape == loaded_array.shape
                 assert (array == loaded_array).all(), (array, '\n', loaded_array)
 
