43dd3d496546cb72d80d75f8d13bb21329564a2f
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2023-02-17 11:54:53 +0100

    test_all now removes spureous persistence dir
    Former-commit-id: ad304da8ddffc58fe1fd04b01f1c73d96176a589

--- test/test_all.py
+++ test/test_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Rull all test modules in the current working dir
+"""Run all test modules in the current working dir
 """
 __author__ = "Miguel Hernández-Cabronero"
 __since__ = "2019/09/19"
@@ -8,26 +8,29 @@ import os
 import glob
 import shutil
 import unittest
-import sys
 import argparse
 import datetime
 
 # So that all tests can use the intended module structure transparently
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="Be verbose? Repeat for more", action="count", default=0)
+parser.add_argument("-v", "--verbose", help="Be verbose? Repeat for more",
+                    action="count", default=0)
 options = parser.parse_known_args()[0]
 
 if __name__ == '__main__':
     # Clean any persistence dirs in test/
     _ = [shutil.rmtree(p)
-         for p in glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), "*.py"))
+         for p in glob.glob(
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), "*.py"))
          if os.path.isdir(p)]
 
     suite = unittest.TestLoader().discover(os.path.dirname(__file__))
 
     if options.verbose:
-        print(f"Running {suite.countTestCases()} tests @ {datetime.datetime.now()}")
+        print(
+            f"Running {suite.countTestCases()} tests "
+            f"@ {datetime.datetime.now()}")
         print(f"{'[Params]':-^30s}")
         for param, value in options.__dict__.items():
             print(f"{param}: {value}")
@@ -35,3 +38,7 @@ if __name__ == '__main__':
         print()
 
     unittest.TextTestRunner(verbosity=3, failfast=True).run(suite)
+
+    shutil.rmtree(os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), "persistence_test_all.py"),
+        ignore_errors=True)
