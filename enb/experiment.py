46e12996f8d1f07460807bdbc5222c1fbcdbd8ab
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2023-04-12 21:09:36 +0200

    Improved enb.experiment.Experiment.get_df() verbose progress report
    Former-commit-id: 32c9457e2048b4c9f2957b6c445389c20eb60676

--- enb/experiment.py
+++ enb/experiment.py
@@ -276,10 +276,10 @@ class Experiment(enb.atable.ATable):
                         f"Computing experiment chunk "
                         f"{chunk_index}/{len(chunks)-1} "
                         f"({100*chunk_index/len(chunks):.2f}%-"
-                        f"{min(100, 100*(chunk_index+1)/len(chunks)):.2f}% "
+                        f"{min(100, 100*(chunk_index+1)/len(chunks)):.2f}%) "
                         f"@ {datetime.datetime.now()}",
                         sep="...\n",
-                        msg_after=f"Completed {self.__class__.__name__} chunk {chunk_index}/{len(chunks)-1}"):
+                        msg_after=f"Completed {self.__class__.__name__} chunk #{chunk_index}/{len(chunks)-1}"):
                     _ = super().get_df(target_indices=chunk,
                                         fill=fill,
                                         overwrite=overwrite,
