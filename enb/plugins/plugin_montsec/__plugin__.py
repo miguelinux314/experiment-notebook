3c79f8c749c99336c2574b8f4ef6fba3d6768315
pauquintastorra <pau.quintas@autonoma.cat>
2023-12-14 14:38:23 +0100

    montsec working

---
+++ enb/plugins/plugin_montsec/__plugin__.py
@@ -0,0 +1,9 @@
+import enb.plugins
+
+
+class MontsecPlugin(enb.plugins.plugin.PluginJava):
+    name = "montsec"
+    label = "Wrapper for the montsec codec"
+    tags = {"data compression", "image", "codec"}
+    contrib_authors = ["Group on Interactive Coding of Images (GICI)"]
+    tested_on = {"linux, MacOS"}
