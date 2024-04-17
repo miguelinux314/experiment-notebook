351b4ac6dcacbbe980f1d470c51cb4db9b860d68
Miguel Hernández-Cabronero <miguel.hernandez@uab.cat>
2024-04-17 15:57:46 +0200

    Added the AC and CCSDS versions of the emporda codec as contrib to extend and thin out enb installations

--- enb/plugins/plugin_emporda/__plugin__.py
+++ enb/plugins/plugin_emporda/__plugin__.py
@@ -1,10 +1,23 @@
+import os
+import zipfile
 import enb.plugins
 
 
 class EmpordaPlugin(enb.plugins.plugin.PluginJava):
     name = "emporda"
-    label = "Wrapper for the emporda codec"
+    label = "Wrapper for the emporda codec (AC and CCSDS versions)"
     tags = {"data compression", "image", "codec"}
     contrib_authors = ["Group on Interactive Coding of Images (GICI)"]
     contrib_reference_urls = ["http://gici.uab.cat/GiciWebPage/downloads.php#emporda"]
+    contrib_download_url_name = [
+        ("https://github.com/miguelinux314/experiment-notebook/blob/dev/contrib/"
+         "emporda_20240419.zip?raw=true", "emporda.zip")]
     tested_on = {"linux"}
+
+    @classmethod
+    def build(cls, installation_dir):
+        """Unzip the folder contents and build the plugin.
+        """
+        with zipfile.ZipFile(os.path.join(installation_dir, "emporda.zip"), "r") as zip_file:
+            zip_file.extractall(installation_dir)
+        super().build(installation_dir)
--- enb/plugins/plugin_emporda/emporda_codec.py
+++ enb/plugins/plugin_emporda/emporda_codec.py
@@ -7,16 +7,19 @@ __since__ = "2022/04/08"
 import os
 import enb.icompression
 import shutil
+import tempfile
 
 
-class Emporda(enb.icompression.LosslessCodec, enb.icompression.NearLosslessCodec,
-              enb.icompression.JavaWrapperCodec, enb.icompression.GiciLibHelper):
-    """Wrapper for the emporda codec (https://gici.uab.cat/GiciWebPage/downloads.php#emporda)
+class EmpordaAC(enb.icompression.LosslessCodec, enb.icompression.NearLosslessCodec,
+                enb.icompression.JavaWrapperCodec, enb.icompression.GiciLibHelper):
+    """Wrapper for the Empordà codec using a custom Arithmetic Coder
+
+    (https://gici.uab.cat/GiciWebPage/downloads.php#emporda)
     """
 
     def __init__(self,
-                 compressor_jar=os.path.join(os.path.dirname(__file__), "emporda.jar"),
-                 decompressor_jar=os.path.join(os.path.dirname(__file__), "emporda.jar"),
+                 compressor_jar=os.path.join(os.path.dirname(__file__), "emporda_ac.jar"),
+                 decompressor_jar=os.path.join(os.path.dirname(__file__), "emporda_ac.jar"),
                  qs=0, ec=1, cm=1, pm=0, wp=2048, up=2):
         """
         :param qs: sets the quantization step.
@@ -47,10 +50,6 @@ class Emporda(enb.icompression.LosslessCodec, enb.icompression.NearLosslessCodec
                          decompressor_jar=decompressor_jar,
                          param_dict=dict(qs=qs, ec=ec, cm=cm, pm=pm, wp=wp, up=up))
 
-    @property
-    def label(self):
-        return "Emporda"
-
     def get_compression_params(self, original_path, compressed_path, original_file_info):
         assert original_file_info["bytes_per_sample"] == 2, \
             f"Only 16-bit samples are currently supported by {self.__class__.__name__}"
@@ -75,7 +74,7 @@ class Emporda(enb.icompression.LosslessCodec, enb.icompression.NearLosslessCodec
             f"Only integer samples are currently supported by {self.__class__.__name__}"
         assert original_file_info["big_endian"], \
             f"Only big-endian samples are currently supported by {self.__class__.__name__}"
-        
+
         return f"-Xmx256g -jar {self.decompressor_jar} -d -i {compressed_path} -o {reconstructed_path} " \
                f"-ig {self.get_gici_geometry_str(original_file_info=original_file_info)} " \
                f"-qs {self.param_dict['qs']} " \
@@ -84,3 +83,99 @@ class Emporda(enb.icompression.LosslessCodec, enb.icompression.NearLosslessCodec
                f"-pm {self.param_dict['pm']} " \
                f"-wp {self.param_dict['wp']} " \
                f"-up {self.param_dict['up']}"
+
+    @property
+    def label(self):
+        return "Emporda AC"
+
+
+class EmpordaCCSDS(enb.icompression.LosslessCodec,
+                   enb.icompression.JavaWrapperCodec,
+                   enb.icompression.GiciLibHelper):
+    """Wrapper for the Empordà codec using the CCSDS 123.0-B-1 entropy coders
+    (block adaptive or sample adaptive), with the possibility of enabling and disabling
+    the prediction stage.
+    (https://gici.uab.cat/GiciWebPage/downloads.php#emporda)
+    """
+    SAMPLE_ADAPTIVE_ENCODER = 0
+    BLOCK_ENCODER = 1
+    OPTIONS_SAMPLE_ADAPTIVE_PATH = os.path.join(
+        os.path.dirname(__file__), "options_sample_adaptive.txt")
+    OPTIONS_BLOCK_ADAPTIVE_PATH = os.path.join(
+        os.path.dirname(__file__), "options_block_adaptive.txt")
+
+    def __init__(
+            self,
+            entropy_coder: int,
+            skip_prediction: bool,
+            compressor_jar: str = os.path.join(os.path.dirname(__file__), "emporda_ccsds.jar"),
+            decompressor_jar: str = os.path.join(os.path.dirname(__file__), "emporda_ccsds.jar")):
+        """
+        :param entropy_coder: one of EmpordaNoPrediction.SAMPLE_ADAPTIVE_ENCODER or
+          EmpordaNoPrediction.BLOCK_ENCODER, for the sample adaptive or the block adaptive
+          entropy coder, respectively.
+        :param skip_prediction: if True, the prediction stage is skipped
+        :param compressor_jar, decompressor_jar: custom jar files implementing the emporda codec.
+        """
+        enb.icompression.JavaWrapperCodec.__init__(
+            self,
+            compressor_jar=compressor_jar,
+            decompressor_jar=decompressor_jar,
+            param_dict={"entropy_coder": entropy_coder,
+                        "skip_prediction": 0 if not skip_prediction else 1})
+
+    def get_compression_params(self, original_path, compressed_path, original_file_info):
+        assert original_file_info["bytes_per_sample"] == 1, \
+            f"Only 8-bit samples are currently supported by {self.__class__.__name__}"
+        assert not original_file_info["float"], \
+            f"Only integer samples are currently supported by {self.__class__.__name__}"
+        assert original_file_info["big_endian"], \
+            f"Only big-endian samples are currently supported by {self.__class__.__name__}"
+
+        if self.param_dict["entropy_coder"] == self.SAMPLE_ADAPTIVE_ENCODER:
+            options_file_path = self.OPTIONS_SAMPLE_ADAPTIVE_PATH
+        elif self.param_dict["entropy_coder"] == self.BLOCK_ENCODER:
+            options_file_path = self.OPTIONS_BLOCK_ADAPTIVE_PATH
+        else:
+            raise ValueError(f"Invalid entropy coder type {self.param_dict['entropy_coder']!r}")
+
+        return (f"-Xmx256g -jar {self.decompressor_jar} "
+                f"-c "
+                f"-i {os.path.abspath(original_path)} "
+                f"-ig {self.get_gici_geometry_str(original_file_info=original_file_info)} "
+                f"-qm 0 "
+                f"-o {os.path.abspath(compressed_path)} "
+                f"-f {options_file_path} "
+                f"-p {'0' if self.param_dict['skip_prediction'] else '1'}")
+
+    def get_decompression_params(self, compressed_path, reconstructed_path, original_file_info):
+        assert original_file_info["bytes_per_sample"] == 1, \
+            f"Only 8-bit samples are currently supported by {self.__class__.__name__}"
+        assert not original_file_info["float"], \
+            f"Only integer samples are currently supported by {self.__class__.__name__}"
+        assert original_file_info["big_endian"], \
+            f"Only big-endian samples are currently supported by {self.__class__.__name__}"
+
+        if self.param_dict["entropy_coder"] == self.SAMPLE_ADAPTIVE_ENCODER:
+            options_file_path = self.OPTIONS_SAMPLE_ADAPTIVE_PATH
+        elif self.param_dict["entropy_coder"] == self.BLOCK_ENCODER:
+            options_file_path = self.OPTIONS_BLOCK_ADAPTIVE_PATH
+        else:
+            raise ValueError(f"Invalid entropy coder type {self.param_dict['entropy_coder']!r}")
+
+        return (f"-Xmx256g -jar {self.decompressor_jar} "
+                f"-d "
+                f"-i {os.path.abspath(compressed_path)} "
+                f"-ig {self.get_gici_geometry_str(original_file_info=original_file_info)} "
+                f"-qm 0 "
+                f"-o {os.path.abspath(reconstructed_path)} "
+                f"-f {options_file_path} "
+                f"-p {'0' if self.param_dict['skip_prediction'] else '1'}")
+
+    @property
+    def label(self):
+        prediction_str = " NP" if self.param_dict["skip_prediction"] == 1 else ""
+        ec_str = f"Sample adaptive" \
+            if self.param_dict["entropy_coder"] == self.SAMPLE_ADAPTIVE_ENCODER \
+            else "Block adaptive"
+        return f"Emporda CCSDS {prediction_str} ({ec_str})"
